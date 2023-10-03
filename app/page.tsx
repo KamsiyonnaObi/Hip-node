4c5fbafa04c2a5429b1ff3465026205dbccbf472
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-03 16:30:52 -0500

    feat: configure typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <div>
-      <h1>Breaking Bugs Hipnode</h1>
+      <h1 className="h1-bold">Breaking Bugs Hipnode</h1>
+      <p className="body-semibold">
+        Welcome to the Breaking Bugs Hipnode! This is a Next.js project with
+        Tailwind CSS.
+      </p>
     </div>
   );
 }
