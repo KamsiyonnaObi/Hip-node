799510d0d79649c1a1a28d7f13b00a4cd55cb658
Lushi Liu <lushiliu1@gmail.com>
2023-10-04 15:21:13 -0400

    changed icons to make it able to dynamically change color depending on theme

--- app/components/icons/outlineIcons/More.tsx
+++
@@ -1,7 +0,0 @@
-export const More = () => (
-  <>
-    <circle cx="2.50415" cy="10" r="2.5" fill="#3F4354" />
-    <circle cx="10.0042" cy="10" r="2.5" fill="#3F4354" />
-    <circle cx="17.5042" cy="10" r="2.5" fill="#3F4354" />
-  </>
-);
