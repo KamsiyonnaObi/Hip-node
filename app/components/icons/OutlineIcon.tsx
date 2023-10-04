ccd9d81e98771cab5e45270f44c29df45666956d
Lushi Liu <lushiliu1@gmail.com>
2023-10-04 14:25:30 -0400

    added outline icons

---
+++ app/components/icons/OutlineIcon.tsx
@@ -0,0 +1,358 @@
+/* eslint-disable react/display-name */
+import { ArrowLeft } from "./outlineIcons/ArrowLeft";
+import { Arrow } from "./outlineIcons/Arrow";
+import { BackArrow } from "./outlineIcons/BackArrow";
+import { Bitcoin } from "./outlineIcons/Bitcoin";
+import { Blogging } from "./outlineIcons/Blogging";
+import { Bold } from "./outlineIcons/Bold";
+import { Checkmark } from "./outlineIcons/Checkmark";
+import { Close } from "./outlineIcons/Close";
+import { Comment } from "./outlineIcons/Comment";
+import { Design } from "./outlineIcons/Design";
+import { Dev } from "./outlineIcons/Dev";
+import { DownArrow } from "./outlineIcons/DownArrow";
+import { Edit } from "./outlineIcons/Edit";
+import { Expand } from "./outlineIcons/Expand";
+import { Facebook } from "./outlineIcons/Facebook";
+import { Follow } from "./outlineIcons/Follow";
+import { Following } from "./outlineIcons/Following";
+import { Headline } from "./outlineIcons/Headline";
+import { Image1 } from "./outlineIcons/Image1";
+import { Image2 } from "./outlineIcons/Image2";
+import { Info } from "./outlineIcons/Info";
+import { Instagram } from "./outlineIcons/Instagram";
+import { Italic } from "./outlineIcons/Italic";
+import { Left } from "./outlineIcons/Left";
+import { LinkIcon } from "./outlineIcons/LinkIcon";
+import { LinkedIn } from "./outlineIcons/LinkedIn";
+import { Mention } from "./outlineIcons/Mention";
+import { Middle } from "./outlineIcons/Middle";
+import { More } from "./outlineIcons/More";
+import { New } from "./outlineIcons/New";
+import { New2 } from "./outlineIcons/New2";
+import { Number } from "./outlineIcons/Number";
+import { Point } from "./outlineIcons/Point";
+import { Popular } from "./outlineIcons/Popular";
+import { Post } from "./outlineIcons/Post";
+import { Right } from "./outlineIcons/Right";
+import { SearchIcon } from "./outlineIcons/SearchIcon";
+import { Seo } from "./outlineIcons/Seo";
+import { Share2 } from "./outlineIcons/Share2";
+import { Strikethrough } from "./outlineIcons/Strikethrough";
+import { Tutorial } from "./outlineIcons/Tutorial";
+import { Twitter } from "./outlineIcons/Twitter";
+import { Uncheck } from "./outlineIcons/Uncheck";
+import { Underline } from "./outlineIcons/Underline";
+import { Upload } from "./outlineIcons/Upload";
+import { Vector } from "./outlineIcons/Vector";
+import { View } from "./outlineIcons/View";
+import { Voice } from "./outlineIcons/Voice";
+import { Web } from "./outlineIcons/Web";
+
+const FillIcon = ({ children }: any) => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    {children}
+  </svg>
+);
+
+FillIcon.ArrowLeft = () => (
+  <FillIcon>
+    <ArrowLeft />
+  </FillIcon>
+);
+
+FillIcon.Arrow = () => (
+  <FillIcon>
+    <Arrow />
+  </FillIcon>
+);
+
+FillIcon.BackArrow = () => (
+  <FillIcon>
+    <BackArrow />
+  </FillIcon>
+);
+
+FillIcon.Bitcoin = () => (
+  <FillIcon>
+    <Bitcoin />
+  </FillIcon>
+);
+
+FillIcon.Blogging = () => (
+  <FillIcon>
+    <Blogging />
+  </FillIcon>
+);
+
+FillIcon.Bold = () => (
+  <FillIcon>
+    <Bold />
+  </FillIcon>
+);
+
+FillIcon.Checkmark = () => (
+  <FillIcon>
+    <Checkmark />
+  </FillIcon>
+);
+
+FillIcon.Close = () => (
+  <FillIcon>
+    <Close />
+  </FillIcon>
+);
+
+FillIcon.Comment = () => (
+  <FillIcon>
+    <Comment />
+  </FillIcon>
+);
+
+FillIcon.Design = () => (
+  <FillIcon>
+    <Design />
+  </FillIcon>
+);
+
+FillIcon.Dev = () => (
+  <FillIcon>
+    <Dev />
+  </FillIcon>
+);
+
+FillIcon.DownArrow = () => (
+  <FillIcon>
+    <DownArrow />
+  </FillIcon>
+);
+
+FillIcon.Edit = () => (
+  <FillIcon>
+    <Edit />
+  </FillIcon>
+);
+
+FillIcon.Expand = () => (
+  <FillIcon>
+    <Expand />
+  </FillIcon>
+);
+
+FillIcon.Facebook = () => (
+  <FillIcon>
+    <Facebook />
+  </FillIcon>
+);
+
+FillIcon.Follow = () => (
+  <FillIcon>
+    <Follow />
+  </FillIcon>
+);
+
+FillIcon.Following = () => (
+  <FillIcon>
+    <Following />
+  </FillIcon>
+);
+
+FillIcon.Headline = () => (
+  <FillIcon>
+    <Headline />
+  </FillIcon>
+);
+
+FillIcon.Image1 = () => (
+  <FillIcon>
+    <Image1 />
+  </FillIcon>
+);
+
+FillIcon.Image2 = () => (
+  <FillIcon>
+    <Image2 />
+  </FillIcon>
+);
+
+FillIcon.Info = () => (
+  <FillIcon>
+    <Info />
+  </FillIcon>
+);
+
+FillIcon.Instagram = () => (
+  <FillIcon>
+    <Instagram />
+  </FillIcon>
+);
+
+FillIcon.Italic = () => (
+  <FillIcon>
+    <Italic />
+  </FillIcon>
+);
+
+FillIcon.Left = () => (
+  <FillIcon>
+    <Left />
+  </FillIcon>
+);
+
+FillIcon.LinkIcon = () => (
+  <FillIcon>
+    <LinkIcon />
+  </FillIcon>
+);
+
+FillIcon.Linkedin = () => (
+  <FillIcon>
+    <LinkedIn />
+  </FillIcon>
+);
+
+FillIcon.Mention = () => (
+  <FillIcon>
+    <Mention />
+  </FillIcon>
+);
+
+FillIcon.Middle = () => (
+  <FillIcon>
+    <Middle />
+  </FillIcon>
+);
+
+FillIcon.More = () => (
+  <FillIcon>
+    <More />
+  </FillIcon>
+);
+
+FillIcon.New = () => (
+  <FillIcon>
+    <New />
+  </FillIcon>
+);
+
+FillIcon.New2 = () => (
+  <FillIcon>
+    <New2 />
+  </FillIcon>
+);
+
+FillIcon.Number = () => (
+  <FillIcon>
+    <Number />
+  </FillIcon>
+);
+
+FillIcon.Point = () => (
+  <FillIcon>
+    <Point />
+  </FillIcon>
+);
+
+FillIcon.Popular = () => (
+  <FillIcon>
+    <Popular />
+  </FillIcon>
+);
+
+FillIcon.Post = () => (
+  <FillIcon>
+    <Post />
+  </FillIcon>
+);
+
+FillIcon.Right = () => (
+  <FillIcon>
+    <Right />
+  </FillIcon>
+);
+
+FillIcon.SearchIcon = () => (
+  <FillIcon>
+    <SearchIcon />
+  </FillIcon>
+);
+
+FillIcon.Seo = () => (
+  <FillIcon>
+    <Seo />
+  </FillIcon>
+);
+
+FillIcon.Share2 = () => (
+  <FillIcon>
+    <Share2 />
+  </FillIcon>
+);
+
+FillIcon.Strikethrough = () => (
+  <FillIcon>
+    <Strikethrough />
+  </FillIcon>
+);
+
+FillIcon.Tutorial = () => (
+  <FillIcon>
+    <Tutorial />
+  </FillIcon>
+);
+
+FillIcon.Twitter = () => (
+  <FillIcon>
+    <Twitter />
+  </FillIcon>
+);
+
+FillIcon.Uncheck = () => (
+  <FillIcon>
+    <Uncheck />
+  </FillIcon>
+);
+
+FillIcon.Underline = () => (
+  <FillIcon>
+    <Underline />
+  </FillIcon>
+);
+
+FillIcon.Upload = () => (
+  <FillIcon>
+    <Upload />
+  </FillIcon>
+);
+
+FillIcon.Vector = () => (
+  <FillIcon>
+    <Vector />
+  </FillIcon>
+);
+
+FillIcon.View = () => (
+  <FillIcon>
+    <View />
+  </FillIcon>
+);
+
+FillIcon.Voice = () => (
+  <FillIcon>
+    <Voice />
+  </FillIcon>
+);
+
+FillIcon.Web = () => (
+  <FillIcon>
+    <Web />
+  </FillIcon>
+);
+
+export default FillIcon;
