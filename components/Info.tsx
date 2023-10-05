efb10ff1caf80c858b6f43e9d94968d655d77682
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-05 13:17:53 -0500

    refactor: make Info component reusable

--- components/Info.tsx
+++ components/Info.tsx
@@ -1,14 +1,18 @@
-import FillIcon from "./icons/FillIcon";
-
-export const Info = () => (
+import React from "react";
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+  fillIcon: any;
+};
+export const Info = ({ children, className, fillIcon }: Props) => (
   <>
     <div className="flex max-w-[402px] items-center gap-5 bg-white dark:bg-dark3">
       <div className="flex rounded-lg bg-red10 p-5 dark:bg-dark4">
-        <FillIcon.Business className="fill-red dark:fill-red90" />
+        {fillIcon}
       </div>
       <div className="flex">
         <p className="h3-semibold text-secondary2 dark:text-secondary6">
-          Connect with other indie hackers running online businesses.
+          {children}
         </p>
       </div>
     </div>
