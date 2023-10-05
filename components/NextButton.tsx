74a58abc0eb3d0eca884535ff58e0fc5c699d238
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-05 09:10:02 -0500

    feat: create Next button Component

---
+++ components/NextButton.tsx
@@ -0,0 +1,12 @@
+export const NextButton = () => {
+  return (
+    <>
+      <button
+        disabled
+        className="h3-semibold rounded-lg bg-red80 px-10 py-2.5 text-background2 disabled:bg-red60"
+      >
+        Next
+      </button>
+    </>
+  );
+};
