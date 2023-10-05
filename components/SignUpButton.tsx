a8767ed6148748f2b809c593cecbcdde0505e912
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-05 14:37:51 -0500

    chore: remove div

--- components/SignUpButton.tsx
+++ components/SignUpButton.tsx
@@ -6,15 +6,13 @@ type Props = {
 };
 export const SignUpButton = ({ children, className, fillIcon }: Props) => (
   <>
-    <div>
-      <button className="rounded-lg bg-secondary6 px-[119px] py-3 font-bold dark:bg-dark4">
-        <div className="flex gap-2.5">
-          <div>{fillIcon}</div>
-          <p className="display-semibold text-secondary2 dark:text-background2">
-            {children}
-          </p>
-        </div>
-      </button>
-    </div>
+    <button className="rounded-lg bg-secondary6 px-[119px] py-3 font-bold dark:bg-dark4">
+      <div className="flex gap-2.5">
+        <div>{fillIcon}</div>
+        <p className="display-semibold text-secondary2 dark:text-background2">
+          {children}
+        </p>
+      </div>
+    </button>
   </>
 );
