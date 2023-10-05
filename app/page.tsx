ff17f868a4ad1aee6efcec90563677e772969b48
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-04 22:59:31 -0500

    feat: fill icons with tailwind classnames

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,28 @@
 import FillIcon from "@/components/icons/FillIcon";
+import OutlineIcon from "@/components/icons/OutlineIcon";
 
 export default function Home() {
   return (
     <div className="m-6">
-      <h1 className="h1-bold">Breaking Bugs Hipnode</h1>
+      <h1 className="h1-bold ">Breaking Bugs Hipnode</h1>
       <p className="body-semibold">
         Welcome to the Breaking Bugs Hipnode! This is a Next.js project with
         Tailwind CSS.
       </p>
       <div className="flex w-[500px] flex-wrap gap-4 py-6">
-        <FillIcon.Rocket />
+        <FillIcon.Rocket className="fill-red-900 stroke-slate-500" />
         <FillIcon.Comment />
         <FillIcon.Calendar />
         <FillIcon.Home />
         <FillIcon.Message />
         <FillIcon.Profile />
-        <FillIcon.Facebook />
-        <FillIcon.Google />
-        <FillIcon.Twitter />
+        <FillIcon.Facebook className="fill-blue stroke-slate-500" />
+        <FillIcon.Google className="fill-blue stroke-green" />
+        <FillIcon.Twitter className="fill-black stroke-white" />
         <FillIcon.Send />
         <FillIcon.Settings />
         <FillIcon.Share />
-        <FillIcon.Thunderbolt />
+        <FillIcon.Thunderbolt className="fill-yellow" />
         <FillIcon.Heart />
         <FillIcon.Reply />
         <FillIcon.Group />
@@ -40,6 +41,9 @@ export default function Home() {
         <FillIcon.Feedback />
         <FillIcon.Podcast />
         <FillIcon.Menu />
+      </div>
+      <div className="flex w-[500px] flex-wrap gap-4 py-6">
+        <OutlineIcon.Bitcoin className="fill-red-900" />
       </div>
     </div>
   );
--- components/icons/FillIcon.tsx
+++ components/icons/FillIcon.tsx
@@ -34,173 +34,180 @@ import {
 
 import React from "react";
 
-type FillIconProps = {
-  children: React.ReactNode;
+interface FillIconProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+const FillIcon = ({ children, className }: FillIconProps) => {
+  const styles =
+    className || "fill-secondary2 dark:fill-background2 w-6 h-6 mr-2";
+  return (
+    <svg
+      width="20"
+      height="20"
+      className={styles}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {children}
+    </svg>
+  );
 };
 
-const FillIcon = ({ children }: FillIconProps) => (
-  <svg
-    className="mr-2 h-6 w-6 fill-black"
-    viewBox="0 0 20 20"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    {children}
-  </svg>
-);
-
-FillIcon.Calendar = () => (
-  <FillIcon>
+FillIcon.Calendar = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Calendar />
   </FillIcon>
 );
-FillIcon.Comment = () => (
-  <FillIcon>
+FillIcon.Comment = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Comment />
   </FillIcon>
 );
-FillIcon.Rocket = () => (
-  <FillIcon>
+FillIcon.Rocket = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Rocket />
   </FillIcon>
 );
-FillIcon.Home = () => (
-  <FillIcon>
+FillIcon.Home = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Home />
   </FillIcon>
 );
-FillIcon.Message = () => (
-  <FillIcon>
+FillIcon.Message = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Message />
   </FillIcon>
 );
-FillIcon.Profile = () => (
-  <FillIcon>
+FillIcon.Profile = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Profile />
   </FillIcon>
 );
-FillIcon.Facebook = () => (
-  <FillIcon>
+FillIcon.Facebook = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Facebook />
   </FillIcon>
 );
-FillIcon.Google = () => (
-  <FillIcon>
+FillIcon.Google = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Google />
   </FillIcon>
 );
-FillIcon.Twitter = () => (
-  <FillIcon>
+FillIcon.Twitter = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Twitter />
   </FillIcon>
 );
-FillIcon.Send = () => (
-  <FillIcon>
+FillIcon.Send = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Send />
   </FillIcon>
 );
-FillIcon.Settings = () => (
-  <FillIcon>
+FillIcon.Settings = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Settings />
   </FillIcon>
 );
-FillIcon.Share = () => (
-  <FillIcon>
+FillIcon.Share = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Share />
   </FillIcon>
 );
-FillIcon.Thunderbolt = () => (
-  <FillIcon>
+FillIcon.Thunderbolt = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Thunderbolt />
   </FillIcon>
 );
-FillIcon.Heart = () => (
-  <FillIcon>
+FillIcon.Heart = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Heart />
   </FillIcon>
 );
-FillIcon.Feedback = () => (
-  <FillIcon>
+FillIcon.Feedback = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Feedback />
   </FillIcon>
 );
-FillIcon.Sun = () => (
-  <FillIcon>
+FillIcon.Sun = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Sun />
   </FillIcon>
 );
-FillIcon.Moon = () => (
-  <FillIcon>
+FillIcon.Moon = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Moon />
   </FillIcon>
 );
-FillIcon.Report = () => (
-  <FillIcon>
+FillIcon.Report = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Report />
   </FillIcon>
 );
-FillIcon.Reply = () => (
-  <FillIcon>
+FillIcon.Reply = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Reply />
   </FillIcon>
 );
-FillIcon.Post = () => (
-  <FillIcon>
+FillIcon.Post = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Post />
   </FillIcon>
 );
-FillIcon.Menu = () => (
-  <FillIcon>
+FillIcon.Menu = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Menu />
   </FillIcon>
 );
-FillIcon.Leave = () => (
-  <FillIcon>
+FillIcon.Leave = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Leave />
   </FillIcon>
 );
-FillIcon.Interviews = () => (
-  <FillIcon>
+FillIcon.Interviews = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Interviews />
   </FillIcon>
 );
-FillIcon.Inbox = () => (
-  <FillIcon>
+FillIcon.Inbox = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Inbox />
   </FillIcon>
 );
-FillIcon.Growing = () => (
-  <FillIcon>
+FillIcon.Growing = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Growing />
   </FillIcon>
 );
-FillIcon.Group = () => (
-  <FillIcon>
+FillIcon.Group = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Group />
   </FillIcon>
 );
-FillIcon.Notifications = () => (
-  <FillIcon>
+FillIcon.Notifications = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Notifications />
   </FillIcon>
 );
-FillIcon.Follow = () => (
-  <FillIcon>
+FillIcon.Follow = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Follow />
   </FillIcon>
 );
-FillIcon.Fire = () => (
-  <FillIcon>
+FillIcon.Fire = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Fire />
   </FillIcon>
 );
-FillIcon.Business = () => (
-  <FillIcon>
+FillIcon.Business = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Business />
   </FillIcon>
 );
-FillIcon.Podcast = () => (
-  <FillIcon>
+FillIcon.Podcast = ({ className }: FillIconProps) => (
+  <FillIcon className={className}>
     <Podcast />
   </FillIcon>
 );
