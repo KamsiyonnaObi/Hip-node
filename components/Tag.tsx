5daad7ce093e9014c2d4c95703635b9d6f2e1b0a
Lushi Liu <lushiliu1@gmail.com>
2023-10-05 15:55:44 -0400

    changed tags and sidebar to match design

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -3,26 +3,26 @@ import React from "react";
 interface Props {
   title: string;
   icon: any;
-  bgcolor: string;
-  postNum: number;
+  className: string;
+  postNum: string;
   desc: string;
 }
 
-const Tag = ({ title, icon, bgcolor, postNum, desc }: Props) => {
+const Tag = ({ title, icon, className, postNum, desc }: Props) => {
   return (
-    <div className="flex flex-row items-center gap-[10px] rounded-[6px] text-secondary4">
-      <div
-        className={`flex h-[28px] w-[28px] items-center justify-center gap-[10px] rounded-[6px] p-[6px] ${bgcolor}`}
+    <article className="flex flex-row items-center gap-[10px] rounded-[6px] text-secondary4">
+      <section
+        className={`flex h-[32px] w-[32px] items-center justify-center gap-[10px] rounded-[6px] p-[6px] ${className}`}
       >
         {icon}
-      </div>
-      <div className="flex flex-col gap-[1px]">
-        <p className="body-semibold ">#{title}</p>
+      </section>
+      <section className="flex flex-col gap-[1px] py-1">
+        <p className="caption-semibold ">#{title}</p>
         <p className="text-sm-regular">
           {postNum} {desc}
         </p>
-      </div>
-    </div>
+      </section>
+    </article>
   );
 };
 
