2a7d47af68098e4853918b6961d8f8f3c74da37e
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-05 13:31:24 -0500

    refactor: info component

--- components/Info.tsx
+++ components/Info.tsx
@@ -7,7 +7,9 @@ type Props = {
 export const Info = ({ children, className, fillIcon }: Props) => (
   <>
     <div className="flex max-w-[402px] items-center gap-5 bg-white dark:bg-dark3">
-      <div className="flex rounded-lg bg-red10 p-5 dark:bg-dark4">
+      <div
+        className={`${className} flex rounded-lg bg-red10 p-5 dark:bg-dark4`}
+      >
         {fillIcon}
       </div>
       <div className="flex">
