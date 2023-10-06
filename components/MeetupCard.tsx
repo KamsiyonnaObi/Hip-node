d62fb8ff3d3c8a401b0194f6b3ac85fab89e92d3
Lushi Liu <lushiliu1@gmail.com>
2023-10-06 15:35:45 -0400

    Initialized Meetups and skeletion of PinndedGroup (TODO later)

---
+++ components/MeetupCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "next/image";
+
+interface Props {
+  month: string;
+  day: number;
+  title: string;
+  desc: string;
+  jobType: string[];
+}
+
+const MeetupCard = ({ month, day, title, desc, jobType }: Props) => {
+  return (
+    <article className="flex flex-row gap-[14px]">
+      <section className="flex flex-col rounded-[6px] border-[1px] border-secondary6 bg-background px-2 py-[10px] text-center">
+        <p className="body-semibold">{month}</p>
+        <h1 className="h1-bold text-blue80">{day}</h1>
+      </section>
+      <section className="flex flex-col gap-[10px]">
+        <p className="body-semibold">{title}</p>
+        <div className="flex flex-row gap-[6px]">
+          <Image src="/MeetupIcon.svg" alt="meetup" width={16} height={16} />
+          <p className="text-sm-semibold text-secondary3">{desc}</p>
+        </div>
+        <div className="flex flex-row gap-[10px]">
+          {jobType.map((type, index) => (
+            <div
+              key={index}
+              className="text-xs-semibold gap-[10px] rounded-[20px] bg-secondary6 px-[8px] py-[2px] text-center text-secondary4"
+            >
+              {type}
+            </div>
+          ))}
+        </div>
+      </section>
+    </article>
+  );
+};
+
+export default MeetupCard;
