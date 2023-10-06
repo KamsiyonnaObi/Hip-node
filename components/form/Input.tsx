dbfa3de74cff2a560d030bc38351b1071a98c089
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-06 14:35:50 -0500

    chore: make width full

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export const Input = ({ onChange, value, placeholder, children }: Props) => {
   return (
-    <div className="flex w-[420px] items-center rounded-lg bg-secondary6 px-5 dark:bg-dark2">
+    <div className="flex w-full items-center rounded-lg bg-secondary6 px-5 dark:bg-dark2">
       <input
         className="w-full rounded-lg bg-secondary6 py-[13px] text-secondary2 outline-none placeholder:text-secondary3 dark:bg-dark2 dark:text-background2 dark:placeholder:text-secondary3"
         placeholder={placeholder || "Type here"}
