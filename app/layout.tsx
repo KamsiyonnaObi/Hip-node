76aad9a4d61318797454884afb2726728352271a
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-06 15:52:40 -0500

    chore: NextAuth progress; created Provider Wraper added GitHub Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-import type { Metadata } from "next";
-import { Source_Sans_3 } from "next/font/google";
 
+import NextAuthProvider from "./NextAuthProvider";
+import { getServerSession } from "next-auth";
+
+import type { Metadata } from "next";
+// eslint-disable-next-line camelcase
+import { Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 
 const SourceSansPro = Source_Sans_3({ subsets: ["latin"] });
@@ -11,14 +15,17 @@ export const metadata: Metadata = {
   description: "Modern Social Media Forum Web App",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession();
   return (
     <html lang="en">
-      <body className={SourceSansPro.className}>{children}</body>
+      <body className={SourceSansPro.className}>
+        <NextAuthProvider session={session}>{children}</NextAuthProvider>
+      </body>
     </html>
   );
 }
