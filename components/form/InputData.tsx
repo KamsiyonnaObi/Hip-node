3f458e6be131b3f5cc8f821ffe0e609b139c84b1
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-06 13:02:24 -0500

    chore: cleanup input component

--- components/form/InputData.tsx
+++ components/form/InputData.tsx
@@ -1,10 +1,13 @@
-export const InputData = () => {
+export const InputData = ({ onChange, value, placeholder, icon }) => {
   return (
-    <>
+    <div className="flex justify-between">
       <input
-        className="w-[420px] rounded-lg bg-secondary6  px-5 py-[13px] text-secondary2 placeholder:text-secondary3 dark:bg-dark2 dark:text-background2  dark:placeholder:text-secondary3"
-        placeholder="e.g. Hipnode123"
+        className="w-[420px] rounded-lg bg-secondary6 px-5 py-[13px] text-secondary2 placeholder:text-secondary3 dark:bg-dark2 dark:text-background2 dark:placeholder:text-secondary3"
+        placeholder={placeholder || "Search"}
+        value={value}
+        onChange={onChange}
       />
-    </>
+      {icon}
+    </div>
   );
 };
