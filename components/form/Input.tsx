87a12109ea647b4ba3515da33a42e2b0e7866682
LushiLiu57 <lushiliu1@gmail.com>
2023-10-07 11:55:22 -0400

    made reusable input compatible for createpost

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -2,16 +2,24 @@ import React from "react";
 type Props = {
   children?: React.ReactNode;
   className?: string;
+  divClassName?: string;
   placeholder?: string;
   value: any;
   onChange: any;
 };
 
-export const Input = ({ onChange, value, placeholder, children }: Props) => {
+export const Input = ({
+  onChange,
+  value,
+  placeholder,
+  children,
+  className,
+  divClassName,
+}: Props) => {
   return (
-    <div className="flex w-full items-center rounded-lg bg-secondary6 px-5 dark:bg-dark2">
+    <div className={`${divClassName}`}>
       <input
-        className="w-full rounded-lg bg-secondary6 py-[13px] text-secondary2 outline-none placeholder:text-secondary3 dark:bg-dark2 dark:text-background2 dark:placeholder:text-secondary3"
+        className={`w-full rounded-lg bg-secondary6 py-[13px] text-secondary2 outline-none placeholder:text-secondary3 dark:bg-dark2 dark:text-background2 dark:placeholder:text-secondary3 ${className}`}
         placeholder={placeholder || "Type here"}
         value={value}
         onChange={onChange}
