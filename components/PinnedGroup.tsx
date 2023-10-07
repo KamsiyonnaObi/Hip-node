cf1a1c12e1353b2b13e3916313c76831b3cf378a
LushiLiu57 <lushiliu1@gmail.com>
2023-10-07 02:13:34 -0400

    changed gap on meetup card

--- components/PinnedGroup.tsx
+++ components/PinnedGroup.tsx
@@ -6,48 +6,48 @@ import Tag from "./Tag";
 const PinnedGroup = () => {
   return (
     <div className="flex w-[210px] flex-col gap-[10px] rounded-[16px] bg-background p-[20px] dark:bg-dark3">
-      <section className="display-semibold mb-5 flex flex-row gap-2">
-        <p>Pinned Group</p>
-        <div className="mt-0.5">
+      <div className="flex flex-col gap-[20px]">
+        <section className="display-semibold mb-5 flex flex-row items-center gap-2">
+          <p>Pinned Group</p>
           <OutlineIcon.ArrowLeft />
+        </section>
+        <div className="flex flex-col gap-[10px] rounded-[16px] dark:bg-dark3">
+          <Tag
+            title="java"
+            icon={<OutlineIcon.Dev className="fill-green" />}
+            desc="placeholder"
+            className="bg-green10"
+            postNum="82,430"
+          />
+          <Tag
+            title="bitcoin"
+            icon={<OutlineIcon.Bitcoin className="fill-blue" />}
+            desc="placeholder"
+            className="bg-blue10"
+            postNum="5,354"
+          />
+          <Tag
+            title="design"
+            icon={<OutlineIcon.Design className="fill-purple" />}
+            desc="placeholder"
+            className="bg-purple10"
+            postNum="5,354"
+          />
+          <Tag
+            title="blogging"
+            icon={<OutlineIcon.Blogging className="fill-primary" />}
+            desc="placeholder"
+            className="bg-red10"
+            postNum="5,354"
+          />
+          <Tag
+            title="tutorial"
+            icon={<OutlineIcon.Tutorial className="fill-yellow" />}
+            desc="placeholder"
+            className="bg-yellow10"
+            postNum="5,354"
+          />
         </div>
-      </section>
-      <div className="flex flex-col rounded-[16px] dark:bg-dark3">
-        <Tag
-          title="java"
-          icon={<OutlineIcon.Dev className="fill-green" />}
-          desc="placeholder"
-          className="bg-green10"
-          postNum="82,430"
-        />
-        <Tag
-          title="bitcoin"
-          icon={<OutlineIcon.Bitcoin className="fill-blue" />}
-          desc="placeholder"
-          className="bg-blue10"
-          postNum="5,354"
-        />
-        <Tag
-          title="design"
-          icon={<OutlineIcon.Design className="fill-purple" />}
-          desc="placeholder"
-          className="bg-purple10"
-          postNum="5,354"
-        />
-        <Tag
-          title="blogging"
-          icon={<OutlineIcon.Blogging className="fill-primary" />}
-          desc="placeholder"
-          className="bg-red10"
-          postNum="5,354"
-        />
-        <Tag
-          title="tutorial"
-          icon={<OutlineIcon.Tutorial className="fill-yellow" />}
-          desc="placeholder"
-          className="bg-yellow10"
-          postNum="5,354"
-        />
       </div>
     </div>
   );
