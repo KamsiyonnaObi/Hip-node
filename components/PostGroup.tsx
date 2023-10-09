b5104c8b5187da07883330e05555c06d811f83d6
LushiLiu57 <lushiliu1@gmail.com>
2023-10-08 20:35:44 -0400

    added post component for group page

---
+++ components/PostGroup.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import FillIcon from "./icons/FillIcon";
+
+interface Props {
+  avatar: string;
+  image: string;
+  title: string;
+  name: string;
+  descTitle: string;
+  desc: string;
+  date: string;
+}
+
+const PostGroup = ({
+  avatar,
+  image,
+  title,
+  name,
+  descTitle,
+  desc,
+  date,
+}: Props) => {
+  const [isHeartClicked, setIsHeartClicked] = useState(false);
+
+  const toggleHeartColor = () => {
+    setIsHeartClicked((prevIsHeartClicked) => !prevIsHeartClicked);
+  };
+
+  return (
+    <article className="mx-auto gap-[10px] rounded-[16px] bg-background p-[10px] dark:bg-dark3 md:w-[248px]">
+      <div className="mx-auto flex flex-col gap-[10px]">
+        <section className="flex flex-row md:gap-[10px]">
+          <Image src={avatar} alt="avatar" width={34} height={34} />
+          <div className="flex flex-col">
+            <p className="caption-semibold">{title}</p>
+            <p className="text-sm-regular">{name}</p>
+          </div>
+        </section>
+        <Image
+          src={image}
+          alt="image"
+          className="w-full md:w-[228px]"
+          width={228}
+          height={106}
+        />
+        <section className="flex w-[110px] flex-row gap-[20px]">
+          <div onClick={toggleHeartColor}>
+            <FillIcon.Heart
+              className={isHeartClicked ? "fill-red80" : "fill-secondary5"}
+            />
+          </div>
+
+          <FillIcon.Message className="fill-secondary5" />
+          <FillIcon.Share className="fill-secondary5" />
+        </section>
+        <section className="flex flex-col gap-[10px]">
+          <p className="body-semibold w-[315px] md:w-[228px]">{descTitle}</p>
+          <p className="caption-regular w-[315px] md:w-[228px]">{desc}</p>
+          <p className="caption-regular text-secondary3">{date}</p>
+        </section>
+      </div>
+    </article>
+  );
+};
+
+export default PostGroup;
