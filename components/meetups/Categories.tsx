e76afd0020b78e7f01e17fbf7502a47ca9de7159
LushiLiu57 <lushiliu1@gmail.com>
2023-10-09 14:01:02 -0400

    made changes to icons to fit design

--- components/meetups/Categories.tsx
+++ components/meetups/Categories.tsx
@@ -25,9 +25,9 @@ const Categories = () => {
   return (
     <article className="rounded-[16px] bg-background dark:bg-dark3 md:w-[210px] md:gap-[10px] md:p-[20px]">
       <div className="w-[331px] gap-[20px] p-[20px] dark:text-background md:gap-[12px] md:p-0">
-        <div className="flex flex-row gap-[193px]">
+        <div className="flex flex-row justify-between">
           <h3 className="h3-semibold ">Categories</h3>
-          <OutlineIcon.DownArrow className="fill-secondary6 dark:fill-secondary3 md:hidden" />
+          <OutlineIcon.DownArrow className="mr-[2px] fill-secondary6 dark:fill-secondary3" />
         </div>
         {categories.map((category, index) => (
           <div
@@ -45,14 +45,14 @@ const Categories = () => {
               onClick={() => toggleCategory(index)}
               className={`${category.checked ? "" : "hidden"}`}
             >
-              <OutlineIcon.Checkmark className="fill-background" />
+              <OutlineIcon.Checkmark className="fill-none stroke-background" />
             </div>
 
             <div
               onClick={() => toggleCategory(index)}
               className={`${category.checked ? "hidden" : ""}`}
             >
-              <OutlineIcon.Uncheck className="fill-background" />
+              <OutlineIcon.Uncheck className="fill-none" />
             </div>
           </div>
         ))}
