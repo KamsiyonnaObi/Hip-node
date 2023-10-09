d0a4aee606043503c3c90caa6e8391cb00c372b3
LushiLiu57 <lushiliu1@gmail.com>
2023-10-09 13:51:19 -0400

    stash changes

--- components/Meetups/MeetupCardBig.tsx
+++ components/Meetups/MeetupCardBig.tsx
@@ -22,17 +22,17 @@ const MeetupCardBig = ({
 }: Props) => {
   return (
     <article className="w-[335px] rounded-[16px] bg-background p-[14px] dark:bg-dark3 md:w-[785px] md:gap-[10px] md:p-5">
-      <div className="flex flex-col  gap-[16px] md:gap-[25px]">
-        <section className="flex flex-row justify-between">
+      <div className="flex flex-col gap-[16px] md:gap-[25px]">
+        <section className="flex flex-row justify-between md:w-[745px]">
           <div className="flex flex-row gap-5">
             <Image
               src={image}
               alt="meetup"
-              width={72}
-              height={72}
-              className="h-[48px] w-[48px] rounded-[6px] md:h-[72px] md:w-[72px]"
+              width={92}
+              height={92}
+              className=" h-[48px] w-[48px] rounded-[6px] shadow-md md:h-[72px] md:w-[72px]"
             />
-            <div className="flex flex-col gap-1 md:gap-2">
+            <div className="flex flex-col justify-center gap-1 md:gap-2">
               <h3 className="md:h3-semibold caption-semibold w-[177px] text-secondary2 dark:text-background2 md:w-full">
                 {title}
               </h3>
@@ -41,7 +41,7 @@ const MeetupCardBig = ({
               </p>
             </div>
           </div>
-          <div className="flex flex-col rounded-[6px] border-[1px] border-secondary5 bg-background px-4 py-[5px] text-center">
+          <div className="flex flex-col justify-center rounded-[6px] border-[1px] border-secondary5 bg-background px-4 py-[5px] text-center shadow-md md:h-[72px] md:w-[59px]">
             <p className="md:body-semibold caption-semibold">{month}</p>
             <h1 className="display-semibold md:h1-bold text-blue80">{day}</h1>
           </div>
@@ -55,7 +55,7 @@ const MeetupCardBig = ({
           {jobType.map((type, index) => (
             <div
               key={index}
-              className="text-xs-semibold gap-[10px] rounded-[20px] bg-secondary6 px-[8px] py-[2px] text-center text-secondary4"
+              className="text-xs-semibold md:caption-semibold gap-[10px] rounded-[20px] bg-secondary6 px-[8px] py-[2px] text-center text-secondary4"
             >
               {type}
             </div>
