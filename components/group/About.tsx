07aa96a6040ef9efd4deb09d92ac77a6a67d7698
LushiLiu57 <lushiliu1@gmail.com>
2023-10-10 18:02:37 -0400

    stash

--- components/group/About.tsx
+++ components/group/About.tsx
@@ -9,7 +9,7 @@ const About = ({ desc }: Props) => {
     <article className="w-[335px] gap-[10px] rounded-[16px] bg-background p-[10px] dark:bg-dark3 md:w-[210px] md:p-[20px]">
       <div className="flex flex-col gap-[10px]">
         <p className="display-semibold">About</p>
-        <p className="caption-regular">{desc}</p>
+        <p className="caption-regular line-clamp-6">{desc}</p>
       </div>
     </article>
   );
