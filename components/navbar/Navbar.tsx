ce4c511f8881434bbe3a10b92ca95a1cca64310d
LushiLiu57 <lushiliu1@gmail.com>
2023-10-10 13:44:30 -0400

    added mobile view of navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,28 +1,52 @@
 "use client";
 import React from "react";
-import { useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import FillIcon from "../icons/FillIcon";
 import { Input } from "../form/Input";
 import OutlineIcon from "../icons/OutlineIcon";
 import { Button } from "../ui/Button";
+import { LogoIcon } from "../icons/LogoIcon";
 
 const Navbar = () => {
-  const router = useRouter();
   const pathname = usePathname();
 
   return (
-    <article className="bg-background dark:bg-dark3 md:gap-[10px] md:px-[40px] md:py-[20px]">
-      <div className="flex flex-row justify-center md:w-[1360px] md:gap-[84px]">
-        <Image
-          src="/logo.svg"
-          alt="logo"
-          width={146}
-          height={38}
-          className="md:gap-[10px]"
-        />
-        <div className="flex flex-row md:gap-[65px]">
+    <article className=" gap-[10px] bg-background px-5 py-3 dark:bg-dark3 md:px-[40px] md:py-[20px]">
+      <div className="flex flex-row justify-center gap-[149px] md:w-[1360px] md:gap-[84px]">
+        <section className="flex flex-row items-center justify-center gap-5 md:gap-2.5">
+          <div className="flex h-[30px] items-center justify-center gap-2.5 rounded-[6px] bg-secondary1 p-1 dark:bg-background">
+            <LogoIcon className="fill-background stroke-background dark:fill-dark2 dark:stroke-dark2" />
+          </div>
+          <h1 className="h1-bold hidden text-primary md:flex">
+            Hipnode
+            <span className="text-secondary1 dark:text-secondary2">.</span>
+          </h1>
+          <OutlineIcon.SearchIcon className="stroke-secondary5 dark:stroke-secondary4 md:hidden" />
+        </section>
+        {/* MOBILE */}
+        <section className="flex flex-row gap-5 md:hidden">
+          <Button className="items-center bg-secondary6 dark:bg-dark4 md:gap-2.5 md:p-2.5">
+            <FillIcon.Message className="fill-secondary4 dark:fill-secondary6" />
+          </Button>
+          <Button className="items-center bg-secondary6 dark:bg-dark4 md:gap-2.5 md:p-2.5">
+            <FillIcon.Notifications className="fill-secondary4 dark:fill-secondary6" />
+          </Button>
+          <div className="flex flex-row items-center justify-center rounded-[8px] border-[1px] border-yellow md:h-[40px] md:w-[40px]">
+            <div className="flex flex-row items-center justify-center rounded-[6px] bg-yellow30 md:h-[34px] md:w-[34px]">
+              <Image
+                className="w-[22px] md:w-[30px]"
+                src="/ExampleAvatar.png"
+                alt="profile"
+                width="30"
+                height="32"
+              />
+            </div>
+          </div>
+        </section>
+        {/* DESKTOP */}
+        <div className="hidden flex-row md:flex md:gap-[65px]">
           <section className="hidden flex-row items-center gap-[20px] bg-background  dark:bg-dark3 md:flex">
             <Link href="/home">
               <Button
@@ -41,7 +65,7 @@ const Navbar = () => {
                 />
               </Button>
             </Link>
-            <Link href="/calendar">
+            <Link href="/meetups">
               <Button
                 className={`items-center  md:gap-2.5 md:p-2.5 ${
                   pathname === "/calendar"
@@ -51,7 +75,7 @@ const Navbar = () => {
               >
                 <FillIcon.Calendar
                   className={`${
-                    pathname === "/calendar"
+                    pathname === "/meetups"
                       ? "fill-background"
                       : "fill-secondary4 dark:fill-secondary6"
                   }`}
