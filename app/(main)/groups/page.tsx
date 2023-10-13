8499deaaac32c30eef22fc7b748a7cc86256a43c
fcvirtual <austin@fcvt.dev>
2023-10-13 12:43:33 -0400

    feat: Meetups & Podcast Comp:

--- app/(main)/groups/page.tsx
+++ app/(main)/groups/page.tsx
@@ -1,10 +1,18 @@
+import Meetups from "@/components/Groups/Meetups";
+import Podcasts from "@/components/Podcasts";
 import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <div>Groups</div>
-    </div>
+    <main className="page-formatting">
+      <section className="flex flex-col md:gap-5"></section>
+      <div className="flex-col">
+        <section className="flex flex-col gap-5">
+          <Meetups />
+          <Podcasts />
+        </section>
+      </div>
+    </main>
   );
 };
 
