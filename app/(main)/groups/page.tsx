fcbef8aa366add4a3d548ef83654cc09c6ca7c5a
fcvirtual <austin@fcvt.dev>
2023-10-13 13:15:25 -0400

    Feat: GroupName Init

--- app/(main)/groups/page.tsx
+++ app/(main)/groups/page.tsx
@@ -1,12 +1,34 @@
 import Podcasts from "@/components/Podcasts";
-import PostGroup from "@/components/groups/PostGroup";
-import Meetups from "@/components/home/Meetups";
+import PostGroup from "@/components/group/PostGroup";
+import Meetups from "@/components/group/Meetups";
 import React from "react";
+import GroupName from "@/components/group/GroupName";
 
 const page = () => {
   return (
     <main className="page-formatting">
+      <section>
+        <GroupName />
+      </section>
       <section className="flex flex-col md:gap-5">
+        <PostGroup
+          avatar={""}
+          image={""}
+          title={"title"}
+          name={"name"}
+          descTitle={"descTitle"}
+          desc={"desc"}
+          date={"OCT/13/2023"}
+        />
+        <PostGroup
+          avatar={""}
+          image={""}
+          title={"title"}
+          name={"name"}
+          descTitle={"descTitle"}
+          desc={"desc"}
+          date={"OCT/13/2023"}
+        />
         <PostGroup
           avatar={""}
           image={""}
