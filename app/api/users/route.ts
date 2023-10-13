cf8664600899595c2a7ed0ba51f0cbea548f5b38
fcvirtual <austin@fcvt.dev>
2023-10-13 12:14:38 -0400

    feat: Setup Mongo & Users

---
+++ app/api/users/route.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth/next";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
