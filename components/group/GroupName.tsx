854d3dd35a33a97a94a03a081c1c9727a865db60
fcvirtual <austin@fcvt.dev>
2023-10-13 13:15:25 -0400

    Feat: GroupName Init

---
+++ components/group/GroupName.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// interface Props {}
+
+const GroupName = () => {
+  return (
+    <div>
+      <div>GroupName</div>
+    </div>
+  );
+};
+
+export default GroupName;
