20dabee1301bd1bae3f448ebcda746770d97475b
fcvirtual <austin@fcvt.dev>
2023-10-13 12:25:16 -0400

    feat: Groups Init

---
+++ app/(main)/groups/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div>Groups</div>
+    </div>
+  );
+};
+
+export default page;
---
+++ components/Groups/PodcastCard.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+import OutlineIcon from "../icons/OutlineIcon";
+
+interface Props {
+  image: string;
+  title: string;
+  author: string;
+}
+
+const PodcastCard = ({ image, title, author }: Props) => {
+  return (
+    <article className="flex flex-row gap-[14px]">
+      <Image src={image} alt="" width={58} height={58} className="shadow-md" />
+      <div className="flex flex-col gap-[6px]">
+        <div className="flex flex-row">
+          <p className="caption-semibold w-[193px]">{title}</p>
+          <OutlineIcon.ArrowLeft className=" mt-4 stroke-secondary3" />
+        </div>
+        <p className="text-xs-regular text-secondary3">by {author}</p>
+      </div>
+    </article>
+  );
+};
+
+export default PodcastCard;
