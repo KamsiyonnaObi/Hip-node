5e1b84fe6b4cb8fa46231ae5693368b6c16a2956
fcvirtual <austin@fcvt.dev>
2023-10-13 15:41:20 -0400

    Feat: Title One

--- components/group/GroupName.tsx
+++ components/group/GroupName.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+import FillIcon from "../icons/FillIcon";
+import OutlineIcon from "../icons/OutlineIcon";
 
 // interface Props {}
 
 const GroupName = () => {
   return (
     <div>
-      <div>GroupName</div>
+      <section>
+        <div className="h-[5rem] w-auto min-w-[11.875rem] shrink-0 gap-[.625rem] rounded-[1rem] bg-secondary1 p-[.63rem] dark:bg-dark3">
+          <div className="flex h-[3.75rem] w-auto shrink-0 justify-between gap-[.625rem] rounded-[.625rem] bg-yellow10 p-[.63rem]">
+            <div className="flex-col">
+              <div className="flex gap-[.38rem]">
+                <FillIcon.Growing className="fill-black" />
+                <h2>Fastest Growing</h2>
+              </div>
+              <div>
+                <p>List updated daily at midnight PST.</p>
+              </div>
+            </div>
+            <div className="my-auto">
+              <OutlineIcon.DownArrow2 />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section></section>
+      <section></section>
     </div>
   );
 };
