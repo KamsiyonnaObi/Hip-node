a3ae8f94b9dcc20b23b79cbed349f9e4c40d66d2
fcvirtual <austin@fcvt.dev>
2023-10-13 15:06:14 -0400

    Feat: Dummy data in Post Component

--- app/(main)/groups/page.tsx
+++ app/(main)/groups/page.tsx
@@ -12,31 +12,37 @@ const page = () => {
       </section>
       <section className="flex flex-col md:gap-5">
         <PostGroup
-          avatar={""}
-          image={""}
-          title={"title"}
-          name={"name"}
-          descTitle={"descTitle"}
-          desc={"desc"}
-          date={"OCT/13/2023"}
+          avatar={"/Avatar.png"}
+          image={"/bird.png"}
+          title={"Browser Extension Makers"}
+          name={"Moinul Hassan"}
+          descTitle={"Database Backups as a Service?"}
+          desc={
+            "I wouldn't mind a daily (or more) backup of my database to be done automatically. I've had a google around, and there are services; but none I've heard of..."
+          }
+          date={"wed, 15 Fabruary 2022"}
         />
         <PostGroup
-          avatar={""}
-          image={""}
-          title={"title"}
-          name={"name"}
-          descTitle={"descTitle"}
-          desc={"desc"}
-          date={"OCT/13/2023"}
+          avatar={"/Avatar.png"}
+          image={"/bird.png"}
+          title={"Browser Extension Makers"}
+          name={"Moinul Hassan"}
+          descTitle={"Database Backups as a Service?"}
+          desc={
+            "I wouldn't mind a daily (or more) backup of my database to be done automatically. I've had a google around, and there are services; but none I've heard of..."
+          }
+          date={"wed, 15 Fabruary 2022"}
         />
         <PostGroup
-          avatar={""}
-          image={""}
-          title={"title"}
-          name={"name"}
-          descTitle={"descTitle"}
-          desc={"desc"}
-          date={"OCT/13/2023"}
+          avatar={"/Avatar.png"}
+          image={"/bird.png"}
+          title={"Browser Extension Makers"}
+          name={"Moinul Hassan"}
+          descTitle={"Database Backups as a Service?"}
+          desc={
+            "I wouldn't mind a daily (or more) backup of my database to be done automatically. I've had a google around, and there are services; but none I've heard of..."
+          }
+          date={"wed, 15 Fabruary 2022"}
         />
       </section>
       <div className="flex-col">
