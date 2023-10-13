80dba2c5d1574476b2f9edb6b3509e65c9ea061b
LushiLiu57 <lushiliu1@gmail.com>
2023-10-13 15:42:45 -0400

    changed background and adjusted button color

--- components/Meetups/HostCard.tsx
+++ components/Meetups/HostCard.tsx
@@ -9,17 +9,14 @@ interface Props {
 
 const HostCard = ({ title, desc, buttonText }: Props) => {
   return (
-    <article className="flex w-[335px] flex-col gap-2.5 rounded-[16px] bg-[#FF7C4D] p-5 text-background">
+    <article className="flex w-[335px] flex-col gap-2.5 rounded-[16px] bg-[#FF7C4D] bg-[url('https://i.ibb.co/ch6kNrt/Host-Meetup.png')] p-5 text-background">
       <div className="flex flex-col gap-5">
         <section className="flex flex-col gap-[6px]">
           <h3 className="h3-semibold">{title}</h3>
           <p className="caption-regular line-clamp-2">{desc}</p>
         </section>
         <section className="flex flex-row gap-[21px]">
-          <Button
-            color="orange"
-            className="body-semibold rounded-[6px] px-4 py-[9px]"
-          >
+          <Button className="body-semibold rounded-[6px] bg-red60 px-4 py-[9px]">
             Code of Conduct
           </Button>
           <Button
