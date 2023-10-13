a5b9b82c81d7d13d9eb476204b2f51b2459824e6
fcvirtual <austin@fcvt.dev>
2023-10-13 12:59:21 -0400

    feat: PostGroup

--- app/(main)/groups/page.tsx
+++ app/(main)/groups/page.tsx
@@ -1,11 +1,22 @@
-import Meetups from "@/components/Groups/Meetups";
 import Podcasts from "@/components/Podcasts";
+import PostGroup from "@/components/groups/PostGroup";
+import Meetups from "@/components/home/Meetups";
 import React from "react";
 
 const page = () => {
   return (
     <main className="page-formatting">
-      <section className="flex flex-col md:gap-5"></section>
+      <section className="flex flex-col md:gap-5">
+        <PostGroup
+          avatar={""}
+          image={""}
+          title={"title"}
+          name={"name"}
+          descTitle={"descTitle"}
+          desc={"desc"}
+          date={"OCT/13/2023"}
+        />
+      </section>
       <div className="flex-col">
         <section className="flex flex-col gap-5">
           <Meetups />
