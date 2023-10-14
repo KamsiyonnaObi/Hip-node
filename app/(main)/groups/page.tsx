bf5081952f9be7536d0e2015f0695b8dad6af0c4
fcvirtual <austin@fcvt.dev>
2023-10-14 13:17:50 -0400

    Feat: Mobile post group"

--- app/(main)/groups/page.tsx
+++ app/(main)/groups/page.tsx
@@ -6,51 +6,55 @@ import GroupName from "@/components/group/GroupName";
 
 const page = () => {
   return (
-    <main className="page-formatting">
+    <main className="page-formatting xs:max-w-[320px] mx-auto sm:max-w-[400px]">
       <section>
-        <GroupName />
+        <div>
+          <GroupName />
+        </div>
       </section>
-      <section className="flex flex-col md:flex-row md:gap-5">
-        <PostGroup
-          avatar={"/Avatar.png"}
-          image={"/bird.png"}
-          title={"Browser Extension Makers"}
-          name={"Moinul Hassan"}
-          descTitle={"Database Backups as a Service?"}
-          desc={
-            "I wouldn't mind a daily (or more) backup of my database to be done automatically. I've had a google around, and there are services; but none I've heard of..."
-          }
-          date={"wed, 15 Fabruary 2022"}
-        />
-        <PostGroup
-          avatar={"/Avatar.png"}
-          image={"/bird.png"}
-          title={"Browser Extension Makers"}
-          name={"Moinul Hassan"}
-          descTitle={"Database Backups as a Service?"}
-          desc={
-            "I wouldn't mind a daily (or more) backup of my database to be done automatically. I've had a google around, and there are services; but none I've heard of..."
-          }
-          date={"wed, 15 Fabruary 2022"}
-        />
-        <PostGroup
-          avatar={"/Avatar.png"}
-          image={"/bird.png"}
-          title={"Browser Extension Makers"}
-          name={"Moinul Hassan"}
-          descTitle={"Database Backups as a Service?"}
-          desc={
-            "I wouldn't mind a daily (or more) backup of my database to be done automatically. I've had a google around, and there are services; but none I've heard of..."
-          }
-          date={"wed, 15 Fabruary 2022"}
-        />
+      <section>
+        <div className="flex flex-col md:flex-row md:gap-5">
+          <PostGroup
+            avatar={"/Avatar.png"}
+            image={"/bird.png"}
+            title={"Browser Extension Makers"}
+            name={"Moinul Hassan"}
+            descTitle={"Database Backups as a Service?"}
+            desc={
+              "I wouldn't mind a daily (or more) backup of my database to be done automatically. I've had a google around, and there are services; but none I've heard of..."
+            }
+            date={"wed, 15 Fabruary 2022"}
+          />
+          <PostGroup
+            avatar={"/Avatar.png"}
+            image={"/bird.png"}
+            title={"Browser Extension Makers"}
+            name={"Moinul Hassan"}
+            descTitle={"Database Backups as a Service?"}
+            desc={
+              "I wouldn't mind a daily (or more) backup of my database to be done automatically. I've had a google around, and there are services; but none I've heard of..."
+            }
+            date={"wed, 15 Fabruary 2022"}
+          />
+          <PostGroup
+            avatar={"/Avatar.png"}
+            image={"/bird.png"}
+            title={"Browser Extension Makers"}
+            name={"Moinul Hassan"}
+            descTitle={"Database Backups as a Service?"}
+            desc={
+              "I wouldn't mind a daily (or more) backup of my database to be done automatically. I've had a google around, and there are services; but none I've heard of..."
+            }
+            date={"wed, 15 Fabruary 2022"}
+          />
+        </div>
       </section>
-      <div className="flex-col">
-        <section className="flex flex-col gap-5">
+      <section>
+        <div className="flex flex-col gap-5">
           <Meetups />
           <Podcasts />
-        </section>
-      </div>
+        </div>
+      </section>
     </main>
   );
 };
