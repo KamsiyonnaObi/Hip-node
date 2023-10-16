cabd318c7b27937fcdb189242b341f3c54494540
fcvirtual <austin@fcvt.dev>
2023-10-16 09:54:03 -0400

    feat: Drop down button

--- components/group/GroupName.tsx
+++ components/group/GroupName.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import FillIcon from "../icons/FillIcon";
 import OutlineIcon from "../icons/OutlineIcon";
 import Image from "next/image";
@@ -6,6 +8,7 @@ import Image from "next/image";
 // interface Props {}
 
 const GroupName = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="mx-auto w-[325px] sm:w-[600px] md:w-[325px] lg:w-[225px]">
       <section>
@@ -25,66 +28,13 @@ const GroupName = () => {
               </div>
             </div>
             <div className="my-auto">
-              <OutlineIcon.DownArrow2 />
+              <button onClick={() => setIsOpen((prev) => !prev)}>
+                {!isOpen ? <OutlineIcon.DownArrow2 /> : <OutlineIcon.Close />}
+              </button>
             </div>
           </div>
         </div>
         {/* Dropdown */}
-        <div className="hidden md:flex md:flex-col">
-          <div className="bg-white dark:bg-dark3">
-            <div className="flex gap-[.5rem] p-[.62rem]">
-              <div className="my-auto">
-                <Image src="/Ellipse.png" alt="icon" width={34} height={34} />
-              </div>
-              <div className="flex-col">
-                <h3 className="caption-regular text-secondary2 dark:text-background2">
-                  Looking to Partner Up
-                </h3>
-                <p className="text-sm-regular text-secondary3">
-                  Share how to succed and w...
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* Demo Two */}
-          <div className="bg-white dark:bg-dark3">
-            <div className="flex gap-[.5rem] p-[.62rem]">
-              <div className="my-auto">
-                <Image src="/Ellipse.png" alt="icon" width={34} height={34} />
-              </div>
-              <div className="flex-col">
-                <h3 className="caption-regular text-secondary2 dark:text-background2">
-                  Looking to Partner Up
-                </h3>
-                <p className="text-sm-regular text-secondary3">
-                  Share how to succed and w...
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* Demo Three */}
-          <div className="bg-white dark:bg-dark3">
-            <div className="flex gap-[.5rem] p-[.62rem]">
-              <div className="my-auto">
-                <Image src="/Ellipse.png" alt="icon" width={34} height={34} />
-              </div>
-              <div className="flex-col">
-                <h3 className="caption-regular text-secondary2 dark:text-background2">
-                  Looking to Partner Up
-                </h3>
-                <p className="text-sm-regular text-secondary3">
-                  Share how to succed and w...
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* See All */}
-          <div className="bg-white p-[.625rem] dark:bg-dark3">
-            <button className="flex h-[.875rem] w-[2.125rem] gap-[.625rem] rounded-[.625rem] bg-purple20 px-[.25rem]">
-              <p className="text-xs-semibold text-purple">See all</p>
-            </button>
-          </div>
-        </div>
       </section>
       {/* Most Popular */}
       <section>
