04d28d5505ca2d2516c2bfa5128b209f89d9cf2f
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-17 07:54:56 -0500

    Fix: Left side

--- components/login/InfoSection.tsx
+++ components/login/InfoSection.tsx
@@ -6,12 +6,12 @@ import { Logo } from "../icons/Logo";
 const InfoSection = () => {
   return (
     <>
-      <div className="mt-[30px] max-md:text-center">
+      <div className="mt-[30px] max-md:text-center md:absolute">
         <Logo />
       </div>
-      <article className="mx-auto max-w-[327px] sm:max-w-[442px]">
+      <article className="mx-auto max-w-[327px] sm:max-w-[442px] md:my-auto">
         <div className="mb-10 max-w-[250px] sm:max-w-[411px]">
-          <h1 className="h3-semibold dark:text-background2 sm:text-[30px] sm:font-bold sm:leading-[40px]">
+          <h1 className="h3-semibold text-secondary2 dark:text-background2 sm:text-[30px] sm:font-bold sm:leading-[40px]">
             Join a thriving community of entrepreneurs and developers.
           </h1>
         </div>
