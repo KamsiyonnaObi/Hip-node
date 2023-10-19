f896c744a7be1a774d060ab3cadb1dea0b0de92f
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-18 21:18:38 -0500

    feature: businessStage

--- components/signup/questionaire/BusinessStage.tsx
+++ components/signup/questionaire/BusinessStage.tsx
@@ -10,31 +10,31 @@ export const BusinessStage = () => {
       <div className="flex w-full flex-col gap-5">
         <Button
           color="gray"
-          className="body-semibold sm:h3-semibold p-4 focus:bg-red80 focus:text-background2 md:bg-secondary6"
+          className="body-semibold sm:h3-semibold btn-focus p-4 focus:bg-red80 focus:text-background2 md:bg-secondary6"
         >
           Considering or planning to start a business
         </Button>
         <Button
           color="gray"
-          className="body-semibold sm:h3-semibold p-4 focus:bg-red80 focus:text-background2 md:bg-secondary6"
+          className="body-semibold sm:h3-semibold btn-focus p-4 focus:bg-red80 focus:text-background2 md:bg-secondary6"
         >
           Actively getting started on something new
         </Button>
         <Button
           color="gray"
-          className="body-semibold sm:h3-semibold p-4 focus:bg-red80 focus:text-background2 md:bg-secondary6"
+          className="body-semibold sm:h3-semibold btn-focus p-4 focus:bg-red80 focus:text-background2 md:bg-secondary6"
         >
           No interest in starting a business
         </Button>
         <Button
           color="gray"
-          className="body-semibold sm:h3-semibold p-4 focus:bg-red80 focus:text-background2 md:bg-secondary6"
+          className="body-semibold sm:h3-semibold btn-focus p-4 focus:bg-red80 focus:text-background2 md:bg-secondary6"
         >
           Earnings from my business fully support me
         </Button>
         <Button
           color="gray"
-          className="body-semibold sm:h3-semibold p-4 focus:bg-red80 focus:text-background2  md:bg-secondary6"
+          className="body-semibold sm:h3-semibold btn-focus p-4 md:bg-secondary6"
         >
           Working on a business, no revenue yet
         </Button>
