7a94905a41121c582fb667874140b19c723b58a9
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-18 21:27:56 -0500

    feature: Coding level

--- components/signup/questionaire/CodeLevel.tsx
+++ components/signup/questionaire/CodeLevel.tsx
@@ -1,5 +1,38 @@
+import { Button } from "@/components/ui/Button";
 import React from "react";
 
 export const CodeLevel = () => {
-  return <div>CodeLevel</div>;
+  return (
+    <>
+      <h1 className="h3-semibold text-secondary2 dark:text-background2 sm:text-[30px] sm:font-bold sm:leading-[40px]">
+        Do you know how to code?
+      </h1>
+      <div className="flex w-full flex-col gap-5">
+        <Button
+          color="gray"
+          className="body-semibold sm:h3-semibold btn-focus p-4 md:bg-secondary6"
+        >
+          No, and coding is totally unfamiliar
+        </Button>
+        <Button
+          color="gray"
+          className="body-semibold sm:h3-semibold btn-focus p-4 md:bg-secondary6"
+        >
+          No, but I understand a few concepts
+        </Button>
+        <Button
+          color="gray"
+          className="body-semibold sm:h3-semibold btn-focus p-4 md:bg-secondary6"
+        >
+          Yes, and I am a beginner
+        </Button>
+        <Button
+          color="gray"
+          className="body-semibold sm:h3-semibold btn-focus p-4 md:bg-secondary6"
+        >
+          Yes, and I&apos;m intermediate or a professional
+        </Button>
+      </div>
+    </>
+  );
 };
