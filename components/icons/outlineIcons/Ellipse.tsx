97de01bfa64d3c980d8a1c9ace318bbca7475544
Austin Margarone <123497938+austinmargarone@users.noreply.github.com>
2023-10-20 08:55:00 -0400

    Update components/icons/outlineIcons/Ellipse.tsx
    Co-authored-by: Brandon Etter <brandonetter@gmail.com>

--- components/icons/outlineIcons/Ellipse.tsx
+++ components/icons/outlineIcons/Ellipse.tsx
@@ -1,13 +1,4 @@
 export const Ellipse = () => (
   <>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="5"
-      height="6"
-      viewBox="0 0 5 6"
-      fill="none"
-    >
-      <circle cx="2.5" cy="3" r="2.5" fill="#C5D0E6" />
-    </svg>
-  </>
+      <circle cx="5" cy="10" r="2.5" fill="#C5D0E6" />
 );
