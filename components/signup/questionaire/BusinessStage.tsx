9828ca0da846315d624e81692906953fe8a993f7
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-20 10:56:40 -0500

    feat: business stage component

--- components/signup/questionaire/BusinessStage.tsx
+++ components/signup/questionaire/BusinessStage.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSignUpStore } from "@/store";
+
 import { Button } from "@/components/ui/Button";
 
 const stages = [
@@ -10,17 +12,24 @@ const stages = [
 ];
 
 export const BusinessStage = () => {
+  const { Stage, updateBusinessStage } = useSignUpStore();
+  const handleButtonClick = (stage: string) => {
+    updateBusinessStage(stage);
+  };
   return (
     <>
       <h1 className="h3-semibold text-secondary2 dark:text-background2 sm:text-[30px] sm:font-bold sm:leading-[40px]">
         Which best describes the stage you&apos;re at right now?
       </h1>
       <div className="flex w-full flex-col gap-5">
-        {stages.map((stage, index) => (
+        {stages.map((stage) => (
           <Button
-            key={index}
+            key={stage}
             color="gray"
-            className="body-semibold sm:h3-semibold btn-focus p-4 md:bg-secondary6"
+            className={`body-semibold sm:h3-semibold p-4 md:bg-secondary6 ${
+              Stage === stage && "selected"
+            }`}
+            onClick={() => handleButtonClick(stage)}
           >
             {stage}
           </Button>
