3fd86ccce922ac2d5fe37699c2d692b0d37191ef
fcvirtual <austin@fcvt.dev>
2023-10-21 12:33:51 -0400

    fix: Component Widths

--- components/group/RecentMedia.tsx
+++ components/group/RecentMedia.tsx
@@ -5,14 +5,14 @@ type Props = { media: string };
 
 const RecentMedia = ({ media }: Props) => {
   return (
-    <div className="flex w-[20.3125rem] flex-col gap-[0.625rem] rounded-[1rem] bg-background p-[1.25rem] dark:bg-dark3">
+    <div className="flex w-[20.9375rem] flex-col gap-[0.625rem] rounded-[1rem] bg-background p-[1.25rem] dark:bg-dark3">
       <div className="mb-[.62rem]">
         <h2 className="display-semibold text-secondary2 dark:text-background2">
           Recent Media
         </h2>
       </div>
       <div className="flex flex-wrap gap-[.62rem]">
-        <div className="w-[5.5rem]">
+        <div className="min-w-[5.5rem]">
           <Image
             src={media}
             width={88}
