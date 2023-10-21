64449102a51260c321f7e7263118b7b7481271f7
fcvirtual <austin@fcvt.dev>
2023-10-21 13:35:16 -0400

    User Types

--- types/mongoose/index.d.ts
+++ types/mongoose/index.d.ts
@@ -5,4 +5,18 @@ export interface IUser {
   username: string;
   email: string;
   password: string;
+  profileImage: string;
+  bannerImage: string;
+  fullName: string;
+  occupation: string;
+  businessType: string;
+  businessStage: string;
+  codingLevel: string;
+  bio: string;
+  socialMedia: string;
+  points: number;
+  following: Schema.Types.ObjectId[];
+  followers: Schema.Types.ObjectId[];
+
+  checkPassword: (password: string | Buffer) => Promise<boolean>;
 }
