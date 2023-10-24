64655e8c1e6df2f6772115be60602d807a5017ca
LushiLiu57 <lushiliu1@gmail.com>
2023-10-24 18:22:30 -0400

    changed default theme to desktop default

--- store/themeStore.tsx
+++ store/themeStore.tsx
@@ -4,9 +4,15 @@ interface ThemeStore {
   theme: string;
   toggleTheme: () => void;
 }
-
+let defaultTheme = "light";
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  defaultTheme = "dark";
+}
 const useThemeState = create<ThemeStore>((set) => ({
-  theme: "light",
+  theme: defaultTheme,
   toggleTheme: () =>
     set((state) => ({
       theme: state.theme === "light" ? "dark" : "light",
