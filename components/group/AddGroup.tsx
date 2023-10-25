72e42c72d8891f057ce274b03962d48b38455f0b
fcvirtual <austin@fcvt.dev>
2023-10-25 15:32:58 -0400

    fix: image1

--- components/group/AddGroup.tsx
+++ components/group/AddGroup.tsx
@@ -13,7 +13,11 @@ const AddGroup = (props: Props) => {
     members: "",
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
@@ -34,8 +38,8 @@ const AddGroup = (props: Props) => {
     <>
       <div className="flex max-w-[20.9375rem] xs:max-w-[30rem] sm:max-w-[38rem] md:max-w-[46rem]  flex-col gap-[1.25em] p-[1.25rem] bg-background dark:bg-dark3 lg:max-w-[55rem] w-full rounded-[1rem] mb-[13.44rem] lg:mb-[5.37rem] mt-[1.25rem] lg:mt-[1.88rem] sm">
         <div className="flex">
-          <button className="w-[5.7rem] flex px-[.625rem] py-[.25rem] gap-[.625rem] items-center rounded-[.25rem] bg-background2 dark:bg-dark4">
-            <OutlineIcon.Image1 className="fill-black dark:fill-white" />
+          <button className="w-[6rem] flex px-[.625rem] py-[.25rem] gap-[.625rem] items-center rounded-[.25rem] bg-background2 dark:bg-dark4">
+            <OutlineIcon.Image1 className="fill-black dark:fill-white w-[22px]" />
             <p className="text-sm-regular text-secondary2 dark:text-background">
               Set Cover
             </p>
@@ -49,7 +53,7 @@ const AddGroup = (props: Props) => {
             <OutlineIcon.Image2 className="fill-white dark:stroke-secondary4 dark:fill-dark4 m-auto mt-[1.2rem]" />
           </div>
           <button className="flex gap-[.62rem] items-center">
-            <OutlineIcon.Image1 className="dark:fill-background2 fill-secondary2" />
+            <OutlineIcon.Image1 className="dark:fill-background2 fill-secondary2 w-[22px]" />
             <p className="text-sm-regular dark:text-background2 text-secondary2">
               Set Profile Photo
             </p>
@@ -75,13 +79,12 @@ const AddGroup = (props: Props) => {
             Description
           </label>
           <div>
-            <input
-              type="text"
+            <textarea
               name="description"
               placeholder="Provide a short description..."
               value={formData.description}
               onChange={handleChange}
-              className="border-background2 dark:border-dark4 caption-regular text-secondary3 dark:bg-dark3 flex h-[6.875rem] w-full min-w-[18.4375rem] max-w-[52.5rem] rounded-[.5rem] border-[2px] px-[1.25rem] py-[.75rem] lg:h-[9rem] top-[0rem]"
+              className="border-background2 dark:border-dark4 caption-regular text-secondary3 dark:bg-dark3 top-[0rem] flex h-[6.875rem] w-full min-w-[18.4375rem] max-w-[52.5rem] rounded-[.5rem] border-[2px] px-[1.25rem] py-[.75rem] lg:h-[9rem]"
             />
           </div>
         </div>
