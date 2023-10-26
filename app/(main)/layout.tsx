ae41c5425c04e355315d966e95cce5b4c694b704
Matthew Gajo <matthew.gajo@gmail.com>
2023-10-26 14:49:37 -0400

    removing current theme provider

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/navbar/Footer";
-import ThemeProvider from "@/providers/ThemeProvider";
 
 export default async function RootLayout({
   children,
@@ -10,12 +9,12 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ThemeProvider>
+    <>
       <Navbar />
       {children}
       <div className="sticky bottom-0 md:hidden">
         <Footer />
       </div>
-    </ThemeProvider>
+    </>
   );
 }
