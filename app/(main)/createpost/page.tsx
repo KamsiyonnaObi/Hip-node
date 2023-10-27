f0f1c458f489f4c82d4973654077819f6e74e212
Asako Hayase <chipmunk.kck@gmail.com>
2023-10-27 14:12:52 -0700

    update: implemented inputpost to a createpost page

---
+++ app/(main)/createpost/page.tsx
@@ -0,0 +1,12 @@
+import { InputPost } from "@/components/form";
+import React from "react";
+
+const page = () => {
+  return (
+    <section className="bg-background2 px-5 pb-7 pt-5 dark:bg-dark2 md:pb-[62px] md:pl-[270px] md:pr-[290px] md:pt-7">
+      <InputPost />
+    </section>
+  );
+};
+
+export default page;
