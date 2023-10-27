1e5ced8a5b1b163802a5840a2e4953848f2acf2a
LushiLiu57 <lushiliu1@gmail.com>
2023-10-27 18:54:37 -0400

    stash

---
+++ components/MessagePopup.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MessagePopup = () => {
+  return (
+    <article className="flex h-[402px] w-[335px] flex-col rounded-[16px] bg-background dark:bg-dark4 md:h-[450px] md:w-[450px]">
+      <div className="ml-5 mt-5 flex flex-col gap-[15px]">
+        <div></div>
+        <hr className="border-secondary6 dark:border-secondary2" />
+      </div>
+    </article>
+  );
+};
+
+export default MessagePopup;
