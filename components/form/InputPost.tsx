57afc8247b5ea0d94653fafe27909f129e155d9d
Asako Hayase <chipmunk.kck@gmail.com>
2023-10-27 14:56:04 -0700

    fix: updated theme for the editor

--- components/form/InputPost.tsx
+++ components/form/InputPost.tsx
@@ -21,8 +21,10 @@ import {
 } from "../ui/form";
 import { Button } from "../ui/Button";
 import OutlineIcon from "../icons/OutlineIcon";
+import useThemeState from "@/store/themeStore";
 
-export function InputPost({ darkMode }: any) {
+export function InputPost() {
+  const { theme } = useThemeState();
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
@@ -154,7 +156,7 @@ export function InputPost({ darkMode }: any) {
                     // @ts-ignore
                     (editorRef.current = editor)
                   }
-                  key={darkMode}
+                  key={theme}
                   initialValue="<p>Tell your story...</p>"
                   init={{
                     height: 376,
@@ -182,7 +184,7 @@ export function InputPost({ darkMode }: any) {
                       "wordcount",
                     ],
                     toolbar_mode: "floating",
-                    skin: darkMode ? "oxide" : "oxide-dark",
+                    skin: theme === "dark" ? "oxide-dark" : "oxide",
                     toolbar:
                       "codeofconduct h1 bold italic underline strikethrough link image alignleft aligncenter " +
                       "alignright bullist numlist",
@@ -192,7 +194,7 @@ export function InputPost({ darkMode }: any) {
                         "alignright bullist numlist",
                       toolbar_mode: "floating",
                     },
-                    content_css: darkMode ? "light" : "dark",
+                    content_css: theme === "dark" ? "dark" : "light",
                     setup: (editor) => {
                       editor.ui.registry.addButton("codeofconduct", {
                         text: "Code of Conduct",
