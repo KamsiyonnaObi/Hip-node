4b19a11a3ed8e379846d5536e97a758b0fbcecfd
Matthew Gajo <matthew.gajo@gmail.com>
2023-10-29 16:13:28 -0400

    removing components.json and fixing errors

--- components/icons/outlineIcons/RightArrow.tsx
+++ components/icons/outlineIcons/RightArrow.tsx
@@ -2,9 +2,9 @@ export const RightArrow = () => {
   return (
     <path
       d="M4 10H16M16 10L11.3333 5M16 10L11.3333 15"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   );
 };
