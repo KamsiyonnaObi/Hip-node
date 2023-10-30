0f2a871256ed1bc09ff5447d2c2d2ab5704ff573
Matthew Gajo <matthew.gajo@gmail.com>
2023-10-30 14:39:04 -0400

    fixing checkmark

--- components/icons/outlineIcons/Checkmark.tsx
+++ components/icons/outlineIcons/Checkmark.tsx
@@ -1,6 +1,12 @@
 export const Checkmark = () => (
-  <>
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <rect width="16" height="16" rx="2" fill="#FF4401" />
     <path d="M4 8.5L7 11L12 5" stroke="white" strokeWidth="1.5" />
-  </>
+  </svg>
 );
