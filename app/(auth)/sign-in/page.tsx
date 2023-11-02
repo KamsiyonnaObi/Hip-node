9bbcff2d913ab09781e80b1559e11525c162f224
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-11-02 06:03:23 -0500

    feat: create sign in page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import InfoSection from "@/components/signup/InfoSection";
-import SignUp from "@/components/signup/Signup";
 import LogInInfoSection from "@/components/signin/LogInInfoSection";
 import SignIn from "@/components/signin/SignIn";
 
@@ -11,11 +9,9 @@ const signIn = () => {
       {/* Info Section */}
       <section className="flex h-full flex-col gap-[60px] bg-background2 px-6 dark:bg-dark2 md:w-1/2">
         <LogInInfoSection />
-        {/* <InfoSection /> */}
       </section>
       {/* Login Section */}
       <section className=" bg-background2 dark:bg-dark2 md:bg-background md:dark:bg-dark3 flex h-full flex-col px-6 pt-20 md:w-1/2 md:py-0 ">
-        {/* <SignUp /> */}
         <SignIn />
       </section>
     </main>
