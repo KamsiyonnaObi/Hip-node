4efa8fbb24ebed8b4ecc8c2e8709affd99574e0d
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-11-02 22:19:23 -0500

    feat: create Credentials provider and authorize

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,62 @@
-import dbConnect from "@/utils/mongooseConnect";
+/* eslint-disable camelcase */
 import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import UserModel from "@/models/User";
+import CredentialsProvider from "next-auth/providers/credentials";
 
+import UserModel from "@/models/User";
+import dbConnect from "@/utils/mongooseConnect";
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  local: boolean;
+}
 interface Profile {
   email: string;
   fullName: string;
   username: string;
   [key: string]: any;
-}  
+}
 
 export const authOptions = {
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(
+        credentials: Record<"email" | "password", string>,
+        req: any
+      ): Promise<User | null> {
+        try {
+          await dbConnect();
+          const { email, password } = credentials;
+          const user = await UserModel.findOne({ email });
+          if (!user) {
+            return null;
+          }
+          const isPasswordValid = await user.checkPassword(password);
+          if (!isPasswordValid) {
+            return null;
+          }
+          const userObj = {
+            id: user._id.toString(),
+            name: user.fullName,
+            email: user.email,
+            local: true,
+          };
+          return userObj;
+        } catch (error) {
+          console.log("Authentication error ", error);
+          return null;
+        }
+      },
+    }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
@@ -24,24 +68,29 @@ export const authOptions = {
   ],
 
   callbacks: {
-    async signIn( profile: Profile) {
-      const { name, email, image } = profile.user
-      const { given_name } = profile.profile
+    async signIn(profile: Profile) {
+      const { name, email, image } = profile.user;
+      const { given_name } = profile.profile;
 
-      try{
+      try {
         await dbConnect();
         let user = await UserModel.findOne({ email });
 
-        if(!user) {
-          user = await UserModel.create({username: name, fullName: given_name, profileImage: image, email});
+        if (!user) {
+          user = await UserModel.create({
+            username: name,
+            fullName: given_name,
+            profileImage: image,
+            email,
+          });
         }
       } catch (e) {
         console.error("Sign-in error:", e);
       }
-      console.log(profile)
+      console.log(profile);
 
       return true;
-    }
+    },
   },
 };
 
