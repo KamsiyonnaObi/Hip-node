e72c5c178047930a34bc6346cddbe5f21326718f
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-11-06 21:44:18 -0600

    chore: display some user data

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { getServerSession } from "next-auth/next";
 
-const Profile = () => {
-  return <div>Profile</div>;
-};
+import { getUserProfile } from "@/utils/actions/user.action";
 
-export default Profile;
+export default async function Profile() {
+  const session = await getServerSession();
+
+  const data = await getUserProfile(session?.user?.email);
+
+  return (
+    <section className="flex h-screen justify-center">
+      <div>
+        <p>Hello Hello!</p>
+        <p> This is {data?.name}'s Profile</p>
+      </div>
+    </section>
+  );
+}
