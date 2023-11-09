4cf287271b76686dc5fdf5ec783c76311bc790d2
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-11-09 07:34:22 -0600

    chore: pass profile data as props

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -7,16 +7,16 @@ import LeftSideBar from "@/components/profile/LeftSideBar";
 export default async function Profile() {
   const session = await getServerSession();
 
-  const data = await getUserProfile(session?.user?.email);
+  const profileData = await getUserProfile(session?.user?.email);
 
   return (
     <section className="flex h-screen justify-center">
       <article className="flex flex-col">
         <div>
           <p>Hello Hello!</p>
-          <p> This is {data?.name}&apos;s Profile</p>
+          <p> This is {profileData?.name}&apos;s Profile</p>
         </div>
-        <LeftSideBar />
+        <LeftSideBar profileData={profileData} />
       </article>
     </section>
   );
