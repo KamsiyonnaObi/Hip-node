21e22e1b8b2153ce720e03a4796f45b641e913a3
Lushi Liu <lushiliu1@gmail.com>
2023-11-17 12:26:48 -0500

    changed navbar to startsWith, fixed footer for mobile

--- components/navbar/Footer.tsx
+++ components/navbar/Footer.tsx
@@ -5,99 +5,52 @@ import React from "react";
 import FillIcon from "../icons/FillIcon";
 import { Button } from "../ui/Button";
 
+const NavbarLink = ({
+  path,
+  pathname,
+  iconName,
+}: {
+  path: string;
+  pathname: string;
+  iconName: string;
+}) => {
+  const DynamicIcon = FillIcon[iconName];
+  return (
+    <Link href={path}>
+      <Button
+        className={`items-center gap-2.5 p-2.5 ${
+          pathname.startsWith(path)
+            ? "bg-primary"
+            : "bg-secondary6  dark:bg-dark4"
+        }`}
+      >
+        <DynamicIcon
+          className={`${
+            pathname.startsWith(path)
+              ? "fill-background"
+              : "fill-secondary4 dark:fill-secondary6"
+          }`}
+        />
+      </Button>
+    </Link>
+  );
+};
+
 const Footer = () => {
   const pathname = usePathname();
   return (
     <article className="sticky top-0 flex flex-row items-center gap-2.5 bg-background px-[27px] py-[14px] dark:bg-dark3">
       <div className="flex flex-row gap-[30px]">
-        <Link href="/home">
-          <Button
-            className={`items-center  gap-2.5 p-2.5 ${
-              pathname === "/home"
-                ? "bg-primary"
-                : "bg-secondary6  dark:bg-dark4"
-            }`}
-          >
-            <FillIcon.Home
-              className={`${
-                pathname === "/home"
-                  ? "fill-background"
-                  : "fill-secondary4 dark:fill-secondary6"
-              }`}
-            />
-          </Button>
-        </Link>
-        <Link href="/meetups">
-          <Button
-            className={`items-center  gap-2.5 p-2.5 ${
-              pathname === "/meetups"
-                ? "bg-primary"
-                : "bg-secondary6  dark:bg-dark4"
-            }`}
-          >
-            <FillIcon.Calendar
-              className={`${
-                pathname === "/meetups"
-                  ? "fill-background"
-                  : "fill-secondary4 dark:fill-secondary6"
-              }`}
-            />
-          </Button>
-        </Link>
+        <NavbarLink path="/home" pathname={pathname} iconName="Home" />
+        <NavbarLink path="/meetups" pathname={pathname} iconName="Calendar" />
+        <NavbarLink path="/groups" pathname={pathname} iconName="Group" />
 
-        <Link href="/group">
-          <Button
-            className={`items-center  gap-2.5 p-2.5 ${
-              pathname === "/groups"
-                ? "bg-primary"
-                : "bg-secondary6  dark:bg-dark4"
-            }`}
-          >
-            <FillIcon.Group
-              className={`${
-                pathname === "/groups"
-                  ? "fill-background"
-                  : "fill-secondary4 dark:fill-secondary6"
-              }`}
-            />
-          </Button>
-        </Link>
-
-        <Link href="/podcast">
-          <Button
-            className={`items-center  gap-2.5 p-2.5 ${
-              pathname === "/podcast"
-                ? "bg-primary"
-                : "bg-secondary6  dark:bg-dark4"
-            }`}
-          >
-            <FillIcon.Podcast
-              className={`${
-                pathname === "/podcast"
-                  ? "fill-background"
-                  : "fill-secondary4 dark:fill-secondary6"
-              }`}
-            />
-          </Button>
-        </Link>
-
-        <Link href="/interview">
-          <Button
-            className={`items-center  gap-2.5 p-2.5 ${
-              pathname === "/interview"
-                ? "bg-primary"
-                : "bg-secondary6  dark:bg-dark4"
-            }`}
-          >
-            <FillIcon.Interviews
-              className={`${
-                pathname === "/interview"
-                  ? "fill-background"
-                  : "fill-secondary4 dark:fill-secondary6"
-              }`}
-            />
-          </Button>
-        </Link>
+        <NavbarLink path="/podcast" pathname={pathname} iconName="Podcast" />
+        <NavbarLink
+          path="/interview"
+          pathname={pathname}
+          iconName="Interviews"
+        />
       </div>
     </article>
   );
