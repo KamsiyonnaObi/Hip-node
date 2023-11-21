54f30e7ad269e986448add0e8035a802d33df547
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-11-20 19:04:14 -0600

    index on Profile-page-layout: 7a1e2b9 Merge branch 'main' into Feature/Profile-page-layout

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,44 +1,76 @@
-import { StartInterview } from "@/components";
-import React from "react";
-// import { getServerSession } from "next-auth/next";
+import {
+  About,
+  ActiveMembers,
+  Admin,
+  Cover,
+  CreateGroup,
+  Frame,
+  Meetups,
+  Post,
+  RecentMedia,
+  StartInterview,
+} from "@/components";
 
-// import { getUserProfile } from "@/utils/actions/user.action";
+import LeftSideBar from "@/components/profile/LeftSideBar";
+
+import React from "react";
+import { getServerSession } from "next-auth/next";
+
+import { getUserProfile } from "@/utils/actions/user.action";
 
 export default async function Profile() {
-  // const session = await getServerSession();
+  const session = await getServerSession();
 
-  // const data = await getUserProfile(session?.user?.email);
+  const data = await getUserProfile(session?.user?.email);
 
   return (
-    <section className="grid-row-6 md:grid-rows-12 mt-5 grid max-w-[1360px] gap-[1.25rem] md:mx-auto md:max-h-[760px] md:grid-cols-[15%_50%_auto] md:gap-5">
-      {/* Admin */}
-      <section className="order-1 row-span-6 mx-auto w-full bg-yellow">
-        <div className="h-[40.5rem] text-white md:h-0">Admin</div>
-      </section>
-      {/* Start Interview */}
-      <section className="col-start-3 row-span-2 mx-auto w-full bg-red">
-        <div className="h-[8rem] text-white md:h-0">
-          <StartInterview />
+    <main className="mx-auto mt-5 flex w-full justify-center">
+      <div className="grid grid-cols-1 gap-[1.25rem] md:grid-cols-[30%_auto] lg:grid-cols-[auto_52%_auto]">
+        {/* Profile */}
+        <section className="md:col-start-1 md:row-span-2 md:row-start-1">
+          <LeftSideBar profileData={data} />
+        </section>
+        {/* Start Your interview */}
+        <section className="md:col-start-2 md:row-span-1 lg:col-start-3">
+          <div>
+            <StartInterview />
+          </div>
+        </section>
+
+        {/* Bar & Post */}
+        <div className=" md:column-start-2 flex flex-col gap-[1.25rem] md:row-auto lg:row-span-2 lg:row-start-1">
+          <section className="">
+            <Frame />
+          </section>
+          <section className=" flex flex-col gap-[1.25rem] max-md:overflow-hidden">
+            <div>
+              <Post
+                postImage={""}
+                title={""}
+                tags={[]}
+                avatar={""}
+                username={""}
+                createdAt={""}
+                views={0}
+                likes={0}
+                comments={0}
+              />
+            </div>
+          </section>
         </div>
-      </section>
-      {/* Bar */}
-      <section className="order-3 row-span-1 mx-auto w-full bg-blue md:order-2 md:h-[80px]">
-        <div className="h-[7.5rem] text-white md:h-0">Bar</div>
-      </section>
-      {/* Post */}
-      <section className="order-4 row-span-5 mx-auto w-full bg-green md:order-3">
-        <div className="h-[8.25rem] text-white md:h-0">Post</div>
-      </section>
-      {/* Meetups */}
-      <section className="order-5 mx-auto w-full bg-black md:hidden">
-        <div className="h-[20.5rem] text-white md:h-0">
-          <p className="text-white">Meetups</p>
-        </div>
-      </section>
-      {/* Podcasts */}
-      <section className="order-6 row-span-4 mx-auto w-full bg-cyan-200">
-        <div className="h-[33.25rem] text-black md:h-0">Podcasts</div>
-      </section>
-    </section>
+
+        {/* Meetups */}
+        <section className="md:hidden">
+          <div>Meetups</div>
+        </section>
+
+        {/* Performance || using meetups for testing */}
+        <section className="md:hidden lg:col-start-3 lg:row-start-2 lg:block">
+          <div>
+            <Meetups />
+          </div>
+        </section>
+      </div>
+    </main>
   );
 }
