3c2d879b64c66b81517fd4860cc799fe0fcbac8b
Lushi Liu <lushiliu1@gmail.com>
2023-11-24 00:38:38 -0500

    stash

---
+++ components/home/PostRender.tsx
@@ -0,0 +1,37 @@
+import { getAllPosts } from "@/utils/actions/post.action";
+import React from "react";
+import Post from "./Post";
+
+const PostRender = async ({
+  searchParams,
+  currentUserId,
+}: {
+  searchParams: URLSearchParams;
+  currentUserId: string;
+}) => {
+  const result = await getAllPosts(searchParams);
+  return (
+    <div className="flex flex-col gap-5">
+      {result.posts.length > 0
+        ? result.posts.map((post) => (
+            <Post
+              key={post._id}
+              _id={post._id.toString()}
+              postImage={post.image}
+              title={post.title}
+              tags={post.tags}
+              avatar={post.avatar}
+              username={post.userId?.username || "unknown"}
+              createdAt={post.createdAt}
+              views={post?.views?.length}
+              likes={post?.likes?.length}
+              comments={post?.comments?.length}
+              showEdit={post.userId?._id.toString() === currentUserId}
+            />
+          ))
+        : "No Posts to Show!"}
+    </div>
+  );
+};
+
+export default PostRender;
