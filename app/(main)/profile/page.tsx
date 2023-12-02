453c428982a3fb65867e1c6da0f4a5f84328e0ed
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-12-02 12:18:12 -0600

    chore: fix profile page layout

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,16 +1,5 @@
-import {
-  About,
-  ActiveMembers,
-  Admin,
-  Cover,
-  CreateGroup,
-  Frame,
-  Meetups,
-  Post,
-  RecentMedia,
-  StartInterview,
-} from "@/components";
-
+import { Frame, Meetups, StartInterview } from "@/components";
+import Post from "@/components/home/Post";
 import LeftSideBar from "@/components/profile/LeftSideBar";
 
 import React from "react";
@@ -22,9 +11,10 @@ export default async function Profile() {
   const session = await getServerSession();
 
   const data = await getUserProfile(session?.user?.email);
+  const dateOfBirth = new Date(1990, 4, 7);
 
   return (
-    <main className="mx-auto mt-5 flex w-full justify-center">
+    <main className="xs:max-w-[320px] mx-auto mt-5 flex w-full max-w-[335px] justify-center sm:max-w-[550px] md:max-w-[500px] xl:max-w-[1100px] lg:max-w-[950px]">
       <div className="grid grid-cols-1 gap-[1.25rem] md:grid-cols-[30%_auto] lg:grid-cols-[auto_52%_auto]">
         {/* Profile */}
         <section className="md:col-start-1 md:row-span-2 md:row-start-1">
@@ -42,7 +32,7 @@ export default async function Profile() {
           <section className="">
             <Frame />
           </section>
-          <section className=" flex flex-col gap-[1.25rem] max-md:overflow-hidden">
+          <section className="flex w-fit flex-col gap-[1.25rem] max-md:overflow-hidden">
             <div>
               <Post
                 postImage={""}
@@ -50,10 +40,11 @@ export default async function Profile() {
                 tags={[]}
                 avatar={""}
                 username={""}
-                createdAt={""}
+                createdAt={dateOfBirth}
                 views={0}
                 likes={0}
                 comments={0}
+                _id={""}
               />
             </div>
           </section>
@@ -61,16 +52,17 @@ export default async function Profile() {
 
         {/* Meetups */}
         <section className="md:hidden">
-          <div>Meetups</div>
+          <Meetups />
         </section>
 
-        {/* Performance || using meetups for testing */}
-        <section className="md:hidden lg:col-start-3 lg:row-start-2 lg:block">
+        {/* Performance */}
+        <section className=" lg:col-start-3 lg:row-start-2 lg:block">
           <div>
-            <Meetups />
+            <div>Performance</div>
           </div>
         </section>
       </div>
+      {/* xs:max-w-[320px] mx-auto sm:max-w-[550px] md:max-w-[700px] xl:max-w-[1100px] lg:max-w-[950px] */}
     </main>
   );
 }
