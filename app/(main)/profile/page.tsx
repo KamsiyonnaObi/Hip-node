5e5841f7f9081b5012b30eed01c884212b8fa7b2
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-12-02 10:25:23 -0600

    chore include all components

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,10 +1,28 @@
+import { Podcasts, StartInterview } from "@/components";
 import LeftSideBar from "@/components/profile/LeftSideBar";
 
 export default async function Profile() {
   return (
-    <section className="flex h-screen justify-center">
+    <section className="">
+      {/* Profile Details */}
       <article className="flex flex-col">
         <LeftSideBar />
+      </article>
+      {/* Start Your Interview */}
+      <article className="flex flex-col">
+        <StartInterview />
+      </article>
+      {/* Bar & Posts */}
+      <article className="flex flex-col">
+        <div>Bar & Posts</div>
+      </article>
+      {/* Meetups */}
+      <article className="flex flex-col">
+        <LeftSideBar />
+      </article>
+      {/* Podcasts */}
+      <article className="flex flex-col">
+        <Podcasts />
       </article>
     </section>
   );
