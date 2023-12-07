a5784c5013f2489616c9d7cc242be4a6ed8dbfda
fcvirtual <austin@fcvt.dev>
2023-12-07 13:39:39 -0500

    feature: Poopular Tags

--- components/home/PinnedGroup.tsx
+++ components/home/PinnedGroup.tsx
@@ -1,46 +1,8 @@
 "use client";
 import React from "react";
 import OutlineIcon from "../icons/OutlineIcon";
-import Tag from "./Tag";
 
 const PinnedGroup = () => {
-  const tagList = [
-    {
-      title: "java",
-      icon: <OutlineIcon.Dev className="fill-green" />,
-      desc: "placeholder",
-      className: "bg-green10",
-      postNum: "82,430",
-    },
-    {
-      title: "bitcoin",
-      icon: <OutlineIcon.Bitcoin className="fill-blue" />,
-      desc: "placeholder",
-      className: "bg-blue10",
-      postNum: "5,354",
-    },
-    {
-      title: "design",
-      icon: <OutlineIcon.Design className="fill-purple" />,
-      desc: "placeholder",
-      className: "bg-purple10",
-      postNum: "5,354",
-    },
-    {
-      title: "blogging",
-      icon: <OutlineIcon.Blogging className="fill-primary" />,
-      desc: "placeholder",
-      className: "bg-red10",
-      postNum: "5,354",
-    },
-    {
-      title: "tutorial",
-      icon: <OutlineIcon.Tutorial className="fill-yellow" />,
-      desc: "placeholder",
-      className: "bg-yellow10",
-      postNum: "5,354",
-    },
-  ];
   return (
     <div className="flex w-[210px] flex-col gap-[10px] rounded-[16px] bg-background p-[20px] dark:bg-dark3">
       <div className="flex flex-col gap-[20px]">
@@ -49,16 +11,9 @@ const PinnedGroup = () => {
           <OutlineIcon.ArrowLeft className=" stroke-secondary3 dark:stroke-background2" />
         </section>
         <div className="flex flex-col gap-[10px] rounded-[16px] dark:bg-dark3">
-          {tagList.map((tagData, index) => (
-            <Tag
-              key={index}
-              title={tagData.title}
-              icon={tagData.icon}
-              desc={tagData.desc}
-              className={tagData.className}
-              postNum={tagData.postNum}
-            />
-          ))}
+          {/* {tagList.map((tagData, index) => (
+            <Tag />
+          ))} */}
         </div>
       </div>
     </div>
--- components/home/Tag.tsx
+++ components/home/Tag.tsx
@@ -1,25 +1,18 @@
 import React from "react";
+import OutlineIcon from "../icons/OutlineIcon";
 
-interface Props {
-  title: string;
-  icon: any;
-  className: string;
-  postNum: string;
-  desc: string;
-}
-
-const Tag = ({ title, icon, className, postNum, desc }: Props) => {
+const Tag = ({ name, count }: any) => {
   return (
     <article className="flex flex-row items-center gap-[10px] rounded-[6px] text-secondary4">
       <section
-        className={`flex h-[32px] w-[32px] items-center justify-center gap-[10px] rounded-[6px] p-[6px] ${className}`}
+        className={`flex h-[32px] w-[32px] items-center justify-center gap-[10px] rounded-[6px] p-[6px]`}
       >
-        {icon}
+        <OutlineIcon.Dev className="fill-green" />
       </section>
       <section className="flex flex-col gap-[1px] py-1">
-        <p className="caption-semibold dark:text-background2">#{title}</p>
+        <p className="caption-semibold dark:text-background2">#{name}</p>
         <p className="text-sm-regular">
-          {postNum} {desc}
+          {count} <span>Posted by this tag</span>
         </p>
       </section>
     </article>
