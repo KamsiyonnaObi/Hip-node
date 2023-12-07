59602cf3d15fb443f9b95c3514498289145fb394
Matthew Gajo <matthew.gajo@gmail.com>
2023-12-07 13:20:21 -0500

    fixing image fallback

--- components/group/Member.tsx
+++ components/group/Member.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { ImageFallback } from "../shared/ImageFallback";
 
 type Props = { profileImage: string; _id: string };
 
@@ -8,7 +8,7 @@ const Member = ({ profileImage, _id }: Props) => {
   return (
     <div className="flex h-[2.5rem] w-[2.5rem] content-center items-center justify-center overflow-hidden rounded-full bg-purple10">
       <Link href={`/profile/${_id}`}>
-        <Image
+        <ImageFallback
           src={profileImage}
           width={34}
           height={34}
