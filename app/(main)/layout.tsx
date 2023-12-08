7d13a79f270123d20a5b13fe4778286a0bab34d6
Brandon Etter <brandonetter@gmail.com>
2023-12-08 10:40:00 -0500

    Update layout.tsx

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,14 +4,8 @@ import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/navbar/Footer";
 import { getCurrentUser } from "@/utils/actions/user.action";
 import { SocketProvider } from "@/providers/SocketProvider";
-<<<<<<< HEAD
-export const dynamic = "force-dynamic";
-=======
-
 
 export const dynamic = "force-dynamic";
-
->>>>>>> a8b425ebeb616142fedf24e0846f1593214bfd41
 export default async function RootLayout({
   children,
 }: {
