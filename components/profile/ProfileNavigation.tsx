c076610ccc8c20ae14c667e1ac1abb218afed5cc
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-12-09 20:57:53 -0600

    feature: profile navigation

--- components/profile/ProfileNavigation.tsx
+++ components/profile/ProfileNavigation.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const ProfileNavigation = () => {
-  return <div>Profile Navigation</div>;
+  return (
+    <Tabs defaultValue="posts" className="w-full">
+      <TabsList className="h-[50px] w-full justify-between gap-x-5 overflow-hidden rounded-[14px] bg-background px-2.5 text-secondary3 hover:overflow-x-auto dark:bg-dark3 dark:text-background lg:h-20 lg:rounded-[20px] lg:px-[30px]">
+        <TabsTrigger value="posts">Posts</TabsTrigger>
+        <TabsTrigger value="meetups">Meetups</TabsTrigger>
+        <TabsTrigger value="podcasts">Podcasts</TabsTrigger>
+        <TabsTrigger value="interviews">Interviews</TabsTrigger>
+        <TabsTrigger value="history">History</TabsTrigger>
+      </TabsList>
+      <TabsContent value="posts">View your Posts here.</TabsContent>
+      <TabsContent value="meetups">View your Meetups here.</TabsContent>
+      <TabsContent value="history">No recent views</TabsContent>
+    </Tabs>
+  );
 };
 
 export default ProfileNavigation;
