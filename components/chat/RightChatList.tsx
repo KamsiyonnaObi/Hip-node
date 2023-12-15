5daebd0de875bb3546ce2e4ff2e5e69ebdb72265
Asako Hayase <chipmunk.kck@gmail.com>
2023-12-15 00:14:25 -0800

    update: add scroll to bottom

--- components/chat/RightChatList.tsx
+++ components/chat/RightChatList.tsx
@@ -1,12 +1,29 @@
 "use client";
+import { useRef, useState, useEffect } from "react";
 
 import { IMessage } from "@/models/message.model";
 import MyMessage from "./MyMessage";
 import OtherUserMesssage from "./OtherUserMessage";
 import { useSocketContext } from "@/providers/SocketProvider";
+import { IUser } from "@/types/mongoose";
 
 const RightChatList = ({ currentUserId }: { currentUserId: string }) => {
   const { messages, currentPartner } = useSocketContext();
+  const [partner, setPartner] = useState<IUser | null>(null);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (partner !== currentPartner) {
+      setPartner(currentPartner);
+
+      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [currentPartner, partner]);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <section className="flex w-full flex-col">
       <div className="flex flex-1 flex-col gap-4 overflow-y-scroll bg-background px-8 pb-8 pt-[21px] dark:bg-dark4">
@@ -41,6 +58,7 @@ const RightChatList = ({ currentUserId }: { currentUserId: string }) => {
           }
           return null;
         })}
+        <div ref={messagesEndRef} />
       </div>
     </section>
   );
