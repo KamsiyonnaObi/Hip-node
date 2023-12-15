3b9a64b80b54b7ecfa6fce435f15cc87a942b63c
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-12-15 07:47:24 -0600

    feat: render user activity with style

--- components/profile/UserHistory.tsx
+++ components/profile/UserHistory.tsx
@@ -1,11 +1,31 @@
 import { getUserHistory } from "@/utils/actions/notification.action";
 import React from "react";
+import UserHistoryCard from "./UserHistoryCard";
 
 type Props = { userId?: string };
 const UserHistory = async ({ userId }: Props) => {
-  const notification = await getUserHistory(userId);
-  console.log(notification);
-  return <div>UserHistory</div>;
+  const notifications = await getUserHistory(userId);
+  //   console.log(notifications);
+  return (
+    <>
+      <section className="flex flex-col gap-2.5">
+        {notifications.length
+          ? notifications.map((notification) => (
+              <div
+                key={notification.title}
+                className=" gap-[10px] rounded-[16px] bg-background p-[14px] dark:bg-dark3 md:gap-[20px] md:p-[20px]"
+              >
+                <UserHistoryCard
+                  username={notification.userTo.username}
+                  type={notification.type}
+                  postedAt={notification.createdAt}
+                />
+              </div>
+            ))
+          : "no recent activity"}
+      </section>
+    </>
+  );
 };
 
 export default UserHistory;
