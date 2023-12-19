8409dc45e8886b4669c2854681bc8b2af8f7a0e0
fcvirtual <austin@fcvt.dev>
2023-12-19 10:27:28 -0500

    feat: Shadow forms

--- components/form/InputPost.tsx
+++ components/form/InputPost.tsx
@@ -145,7 +145,7 @@ export function InputPost({
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="rounded-2xl bg-background p-5 dark:bg-dark3"
+        className="rounded-2xl bg-background p-5 shadow-lg dark:bg-dark3"
       >
         <FormField
           control={form.control}
