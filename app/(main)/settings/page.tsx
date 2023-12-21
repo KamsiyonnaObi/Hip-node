9460d5f9807c55a6dd7a00455e852cd8dbd81e67
fcvirtual <austin@fcvt.dev>
2023-12-21 11:50:45 -0500

    feat: settings page

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -7,7 +7,7 @@ const page = async () => {
 
   return (
     <section className=" xs:max-w-[320px] mx-auto max-w-[325px] sm:max-w-[550px] md:max-w-[700px] lg:max-w-[850px]">
-      <div className="mt-[1.25rem]">
+      <div className="my-[1.25rem]">
         <ProfileDetails JSONProfileData={JSON.stringify(profileData)} />
       </div>
     </section>
