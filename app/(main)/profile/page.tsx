0d79d22695380d428461b5b086be53e82acee6f7
fcvirtual <austin@fcvt.dev>
2023-12-22 10:30:29 -0500

    fix: Drop Extra Meetup Comp

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,10 +1,11 @@
-import { Meetups, StartInterview } from "@/components";
+import { StartInterview } from "@/components";
 
 import ProfileNavigation from "@/components/profile/ProfileNavigation";
 import ProfileDetails from "@/components/profile/ProfileDetails";
 
 import { getCurrentUser } from "@/utils/actions/user.action";
 import React from "react";
+import Meetups from "@/components/home/Meetups";
 
 export default async function Profile() {
   const profileData = await getCurrentUser(["followers"]);
