271705802cc7aa3a3de7cd29a8e0718ee2112a8e
fcvirtual <austin@fcvt.dev>
2024-01-08 09:11:50 -0500

    fix: Route to Page

--- app/(auth)/info/privacy-policy/page.tsx
+++ app/(auth)/info/privacy-policy/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const route = () => {
-  return <div>privacy policy</div>;
+  return <div>privacy policy Page</div>;
 };
 
 export default route;
--- app/(auth)/info/terms-of-service/page.tsx
+++ app/(auth)/info/terms-of-service/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const route = () => {
-  return <div>terms-of-service</div>;
+  return <div>terms-of-service Page</div>;
 };
 
 export default route;
